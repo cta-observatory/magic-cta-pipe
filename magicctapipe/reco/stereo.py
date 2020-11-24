0de6b4478e9701aee89ba788b6e46cca0e818490
Davide Depaoli <davide.depaoli@to.infn.it>
2020-11-24 19:00:36 +0100

    Add StereoInfoContainer

--- magicctapipe/reco/stereo.py
+++ magicctapipe/reco/stereo.py
@@ -1,8 +1,12 @@
 import numpy as np
+import astropy.units as u
+
+from ctapipe.core.container import Container, Field
+
 
 def check_write_stereo(event, tel_id, computed_hillas_params, hillas_reco,
-                        subarray, array_pointing, telescope_pointings,
-                        event_info, writer):
+                       subarray, array_pointing, telescope_pointings,
+                       event_info, writer):
     """Check hillas parameters and write stero parameters
 
     Parameters
@@ -52,3 +56,16 @@ def check_write_stereo(event, tel_id, computed_hillas_params, hillas_reco,
             # Storing the result
             writer.write("stereo_params", (event_info, stereo_params))
     return stereo_params
+
+
+class StereoInfoContainer(Container):
+    """"InfoContainer fro stereo"""    
+    obs_id = Field(-1, "Observation ID")
+    event_id = Field(-1, "Event ID")
+    tel_id = Field(-1, "Telescope ID")
+    true_energy = Field(-1, "MC event energy", unit=u.TeV)
+    true_alt = Field(-1, "MC event altitude", unit=u.rad)
+    true_az = Field(-1, "MC event azimuth", unit=u.rad)
+    tel_alt = Field(-1, "MC telescope altitude", unit=u.rad)
+    tel_az = Field(-1, "MC telescope azimuth", unit=u.rad)
+    num_islands = Field(-1, "Number of image islands")
