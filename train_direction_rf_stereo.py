ee60a61562922ff5e8361fca5e790112b33af74d
Davide Depaoli <davide.depaoli@to.infn.it>
2020-11-29 11:53:06 +0100

    Maybe working on simtel, to check

--- train_direction_rf_stereo.py
+++ train_direction_rf_stereo.py
@@ -10,15 +10,10 @@ import sklearn
 import sklearn.ensemble
 from astropy import units as u
 from astropy.coordinates import SkyCoord, AltAz
-from astropy.coordinates.angle_utilities import angular_separation, position_angle
+from astropy.coordinates.angle_utilities import position_angle
+from astropy.coordinates.angle_utilities import angular_separation
 from matplotlib import colors
 import matplotlib.pyplot as plt
-
-import ctapipe
-from ctapipe.instrument import CameraGeometry
-from ctapipe.instrument import TelescopeDescription
-from ctapipe.instrument import OpticsDescription
-from ctapipe.instrument import SubarrayDescription
 
 from magicctapipe.utils.plot import *
 from magicctapipe.utils.tels import *
@@ -26,34 +21,39 @@ from magicctapipe.utils.utils import *
 from magicctapipe.utils.filedir import *
 from magicctapipe.train.event_processing import DirectionEstimatorPandas
 
+# import ctapipe
+# from ctapipe.instrument import CameraGeometry
+# from ctapipe.instrument import TelescopeDescription
+# from ctapipe.instrument import OpticsDescription
+# from ctapipe.instrument import SubarrayDescription
+
 PARSER = argparse.ArgumentParser(
-    description="This tools fits the event classification random forest on "
-    "the specified events files. For stereo data.",
+    description=("This tools fits the event classification random forest on "
+                 "the specified events files. For stereo data."),
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 PARSER.add_argument('-cfg', '--config_file', type=str, required=True,
                     help='Configuration file to steer the code execution')
 
 
-def compute_separation_angle(shower_data):
+def compute_separation_angle(shower_data_test):
     separation = dict()
-
-    tel_ids = get_tel_ids(shower_data)
+    tel_ids = get_tel_ids(shower_data_test)
 
     for tel_id in tel_ids:
         event_coord_true = SkyCoord(
-            shower_data_test.loc[(slice(None), slice(None), tel_id),
-                                 'true_az'].values * u.rad,
-            shower_data_test.loc[(slice(None), slice(None), tel_id),
-                                 'true_alt'].values * u.rad,
+            shower_data_test.loc[
+                (slice(None), slice(None), tel_id), 'true_az'].values * u.rad,
+            shower_data_test.loc[
+                (slice(None), slice(None), tel_id), 'true_alt'].values * u.rad,
             frame=AltAz()
         )
 
         event_coord_reco = SkyCoord(
-            shower_data_test.loc[(slice(None), slice(None), tel_id),
-                                 'az_reco'].values * u.rad,
-            shower_data_test.loc[(slice(None), slice(None), tel_id),
-                                 'alt_reco'].values * u.rad,
+            shower_data_test.loc[
+                (slice(None), slice(None), tel_id), 'az_reco'].values * u.rad,
+            shower_data_test.loc[
+                (slice(None), slice(None), tel_id), 'alt_reco'].values * u.rad,
             frame=AltAz()
         )
 
@@ -75,15 +75,18 @@ def compute_separation_angle(shower_data):
 
     # Converting to a data frame
     separation_df = pd.DataFrame(
-        data={'sep_0': separation[0]}, index=shower_data_test.index)
-    for tel_id in separation_df.index.levels[2]:
+        data={'sep_0': separation[0]},
+        index=shower_data_test.index
+    )
+    # for tel_id in separation_df.index.levels[2]: # OLD
+    for tel_id in tel_ids:
         df = pd.DataFrame(
             data={f'sep_{tel_id:d}': separation[tel_id]},
-            index=shower_data_test.loc[(slice(None), slice(None), tel_id),
-                                       'true_az'].index)
+            index=shower_data_test.loc[
+                (slice(None), slice(None), tel_id), 'true_az'].index)
         separation_df = separation_df.join(df)
 
-    separation_df = separation_df.join(shower_data)
+    separation_df = separation_df.join(shower_data_test)
 
     for tel_id in [0]+tel_ids:
         print(f"  Tel {tel_id} scatter: ",
@@ -122,17 +125,10 @@ def get_weights(mc_data, alt_edges, intensity_edges):
 # === Main code ===
 # =================
 def train_direction_rf_stereo(config_file):
-    # ------------------------------
+    # Load config_file
     cfg = load_cfg_file_check(config_file=config_file, label='direction_rf')
-    # ------------------------------
 
-    # MAGIC telescope positions in m wrt. to the center of CTA simulations
-    # magic_tel_positions = {
-    #     1: [-27.24, -146.66, 50.00] * u.m,
-    #     2: [-96.44, -96.77, 51.00] * u.m
-    # }
-
-    # --- MAGIC description ----------------------------------------------------
+    # --- MAGIC description ---
     # magic_optics = OpticsDescription.from_name('MAGIC')
     # magic_cam = CameraGeometry.from_name('MAGICCam')
     # magic_tel_description = TelescopeDescription(name='MAGIC',
@@ -150,7 +146,7 @@ def train_direction_rf_stereo(config_file):
     #     tel_descriptions=magic_tel_descriptions
     # )
 
-    # --- LST description ------------------------------------------------------
+    # --- LST description ---
     # lst_optics = OpticsDescription.from_name('LST')
     # lst_cam = CameraGeometry.from_name('LSTCam')
     # lst_tel_description = TelescopeDescription(name='LST',
@@ -170,7 +166,7 @@ def train_direction_rf_stereo(config_file):
     #     tel_descriptions=lst_tel_descriptions
     # )
 
-    # --- MAGIC - LST description ----------------------------------------------
+    # --- MAGIC - LST description ---
     array_tel_descriptions = {
         **get_tel_descriptions(
             name='LST', cam='LSTCam', tel_ids=cfg['LST']['tel_ids']),
@@ -179,24 +175,6 @@ def train_direction_rf_stereo(config_file):
     }
 
     # --- Train sample ---
-    # info_message('Loading M1 train data...', prefix='DirRF')
-    # hillas_data_m1 = pd.read_hdf(
-    #     cfg['data_files']['mc']['train_sample']['magic1']['hillas_output'],
-    #     key='dl1/hillas_params')
-    # hillas_data_m1.set_index(['obs_id', 'event_id', 'tel_id'], inplace=True)
-    # hillas_data_m1.sort_index(inplace=True)
-
-    # info_message('Loading M2 train data...', prefix='DirRF')
-    # hillas_data_m2 = pd.read_hdf(
-    #     cfg['data_files']['mc']['train_sample']['magic2']['hillas_output'],
-    #     key='dl1/hillas_params')
-    # hillas_data_m2.set_index(['obs_id', 'event_id', 'tel_id'], inplace=True)
-    # hillas_data_m2.sort_index(inplace=True)
-
-    # info_message('Merging the data sets...', prefix='DirRF')
-    # shower_data_train = hillas_data_m1.append(hillas_data_m2)
-    # shower_data_train.sort_index(inplace=True)
-
     f_ = cfg['data_files']['mc']['train_sample']['hillas_h5']
     shower_data_train = load_dl1_data(f_)
 
@@ -211,26 +189,9 @@ def train_direction_rf_stereo(config_file):
     shower_data_train = shower_data_train.join(mc_weights)
 
     # --- Test sample ---
-    # info_message('Loading M1 test data...', prefix='DirRF')
-    # hillas_data_m1 = pd.read_hdf(
-    #     cfg['data_files']['mc']['test_sample']['magic1']['hillas_output'],
-    #     key='dl1/hillas_params')
-    # hillas_data_m1.set_index(['obs_id', 'event_id', 'tel_id'], inplace=True)
-    # hillas_data_m1.sort_index(inplace=True)
-
-    # info_message('Loading M2 test data...', prefix='DirRF')
-    # hillas_data_m2 = pd.read_hdf(
-    #     cfg['data_files']['mc']['test_sample']['magic2']['hillas_output'],
-    #     key='dl1/hillas_params')
-    # hillas_data_m2.set_index(['obs_id', 'event_id', 'tel_id'], inplace=True)
-    # hillas_data_m2.sort_index(inplace=True)
-
-    # info_message('Merging the data sets...', prefix='DirRF')
-    # shower_data_test = hillas_data_m1.append(hillas_data_m2)
-    # shower_data_test.sort_index(inplace=True)
-
     f_ = cfg['data_files']['mc']['test_sample']['hillas_h5']
     shower_data_test = load_dl1_data(f_)
+    tel_ids = get_tel_ids(shower_data_test)
 
     # --- Data preparation ---
     l_ = ['obs_id', 'event_id']
@@ -256,8 +217,6 @@ def train_direction_rf_stereo(config_file):
                                           cfg['direction_rf']['joblib_name']))
     # direction_estimator.load(cfg['direction_rf']['save_name'])
 
-    tel_ids = get_tel_ids(shower_data_test)
-
     # Printing the parameter "importances"
     for kind in direction_estimator.telescope_rfs:
         rfs_ = direction_estimator.telescope_rfs[kind]
@@ -277,6 +236,8 @@ def train_direction_rf_stereo(config_file):
     # --- Evaluating the performance ---
     info_message('Evaluating the performance', prefix='DirRF')
     separation_df = compute_separation_angle(shower_data_test)
+    global s
+    s = separation_df
 
     # Energy-dependent resolution
     info_message('Estimating the energy-dependent resolution', prefix='DirRF')
@@ -302,9 +263,13 @@ def train_direction_rf_stereo(config_file):
                 tel_id = pi
             else:
                 tel_id = 1
-            selection = query.loc[(slice(None), slice(None), tel_id),
-                                  f'sep_{pi}'].dropna()
-            energy_psf[pi][ei] = np.percentile(selection, 68)
+            try:
+                selection = query.loc[
+                    (slice(None), slice(None), tel_id), f'sep_{pi}'].dropna()
+                energy_psf[pi][ei] = np.percentile(selection, 68)
+            except Exception as e:
+                print("ERROR: %s. Setting energy_psf to 0" % e)
+                energy_psf[pi][ei] = 0
 
     # Offset-dependent resolution
     info_message('Estimating the offset-dependent resolution', prefix='DirRF')
@@ -337,9 +302,13 @@ def train_direction_rf_stereo(config_file):
                 tel_id = pi
             else:
                 tel_id = 1
-            selection = query.loc[(slice(None), slice(None), tel_id),
-                                  [f'sep_{pi}']].dropna()
-            offset_psf[pi][oi] = np.percentile(selection[f'sep_{pi}'], 68)
+            try:
+                selection = query.loc[
+                    (slice(None), slice(None), tel_id), [f'sep_{pi}']].dropna()
+                offset_psf[pi][oi] = np.percentile(selection[f'sep_{pi}'], 68)
+            except Exception as e:
+                print("ERROR: %s. Setting offset_psf to 0" % e)
+                offset_psf[pi][oi] = 0
 
     # ================
     # === Plotting ===
@@ -352,7 +321,7 @@ def train_direction_rf_stereo(config_file):
 
     # for tel_id in [0, 1, 2]:
     for tel_id in [0]+tel_ids:
-        plt.subplot2grid((3, 2), (tel_id, 0))
+        plt.subplot2grid((len([0]+tel_ids), 2), (tel_id, 0))
         plt.title(f'Tel {tel_id}')
         plt.xlabel(r'$\theta^2$, deg$^2$')
         # plt.semilogy()
@@ -362,13 +331,13 @@ def train_direction_rf_stereo(config_file):
                  range=(0, 0.5), density=True, histtype='step', color='C0')
         plt.grid(linestyle=':')
 
-        plt.subplot2grid((3, 2), (tel_id, 1))
+        plt.subplot2grid((len([0]+tel_ids), 2), (tel_id, 1))
         plt.xlabel(r'$\theta$, deg')
         plt.xlim(0, 2.0)
-        plt.hist(separation_df[f'sep_{tel_id}'], bins=400, range=(
-            0, 5), cumulative=True, density=True, alpha=0.1, color='C0')
-        plt.hist(separation_df[f'sep_{tel_id}'], bins=400, range=(
-            0, 5), cumulative=True, density=True, histtype='step', color='C0')
+        plt.hist(separation_df[f'sep_{tel_id}'], bins=400, range=(0, 5),
+                 cumulative=True, density=True, alpha=0.1, color='C0')
+        plt.hist(separation_df[f'sep_{tel_id}'], bins=400, range=(0, 5),
+                 cumulative=True, density=True, histtype='step', color='C0')
         plt.grid(linestyle=':')
 
     plt.tight_layout()
@@ -378,13 +347,19 @@ def train_direction_rf_stereo(config_file):
     plt.close()
 
     plt.clf()
+
+    tel_labels = ['LST', 'MAGIC']
     plt.semilogx()
     plt.xlabel('Energy [TeV]')
     plt.ylabel(r'$\sigma_{68}$ [deg]')
     plt.ylim(0, 1.0)
     plt.plot(energy, energy_psf[0], linewidth=4, label='Total')
-    plt.plot(energy, energy_psf[1], label='M1')
-    plt.plot(energy, energy_psf[2], label='M2')
+    for tel_id in tel_ids:
+        for tel_label in tel_labels:
+            if(tel_id in cfg[tel_label]['tel_ids']):
+                l_ = 'M' if(tel_label == "MAGIC") else tel_label
+                tel_n = '%s%d' % (l_, tel_id-cfg[tel_label]['tel_ids'][0]+1)
+                plt.plot(energy, energy_psf[tel_id], label=tel_n)
     plt.grid(linestyle=':')
     plt.legend()
     save_plt(n=cfg['direction_rf']['fig_name_PSF_energy'],
@@ -397,8 +372,12 @@ def train_direction_rf_stereo(config_file):
     plt.ylabel(r'$\sigma_{68}$ [deg]')
     plt.ylim(0, 0.5)
     plt.plot(offset, offset_psf[0], linewidth=4, label='Total')
-    plt.plot(offset, offset_psf[1], label='M1')
-    plt.plot(offset, offset_psf[2], label='M2')
+    for tel_id in tel_ids:
+        for tel_label in tel_labels:
+            if(tel_id in cfg[tel_label]['tel_ids']):
+                l_ = 'M' if(tel_label == "MAGIC") else tel_label
+                tel_n = '%s%d' % (l_, tel_id-cfg[tel_label]['tel_ids'][0]+1)
+                plt.plot(offset, offset_psf[tel_id], label=tel_n)
     plt.grid(linestyle=':')
     plt.legend()
     save_plt(n=cfg['direction_rf']['fig_name_PSF_offset'],
