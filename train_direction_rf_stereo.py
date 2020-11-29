61170dfb3d8b4ab57632b9f3b549b281f02a7a2f
Davide Depaoli <davide.depaoli@to.infn.it>
2020-11-29 13:02:08 +0100

    Mod on desc

--- train_direction_rf_stereo.py
+++ train_direction_rf_stereo.py
@@ -19,6 +19,7 @@ from magicctapipe.utils.plot import *
 from magicctapipe.utils.tels import *
 from magicctapipe.utils.utils import *
 from magicctapipe.utils.filedir import *
+from magicctapipe.train.utils import *
 from magicctapipe.train.event_processing import DirectionEstimatorPandas
 
 # import ctapipe
@@ -28,7 +29,7 @@ from magicctapipe.train.event_processing import DirectionEstimatorPandas
 # from ctapipe.instrument import SubarrayDescription
 
 PARSER = argparse.ArgumentParser(
-    description=("This tools fits the event classification random forest on "
+    description=("This tools fits the direction random forest regressor on "
                  "the specified events files. For stereo data."),
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
@@ -95,32 +96,6 @@ def compute_separation_angle(shower_data_test):
     return separation_df
 
 
-def get_weights(mc_data, alt_edges, intensity_edges):
-    mc_hist, _, _ = np.histogram2d(mc_data['tel_alt'],
-                                   mc_data['intensity'],
-                                   bins=[alt_edges, intensity_edges])
-
-    availability_hist = np.clip(mc_hist, 0, 1)
-
-    # --- MC weights ---
-    mc_alt_bins = np.digitize(mc_data['tel_alt'], alt_edges) - 1
-    mc_intensity_bins = np.digitize(mc_data['intensity'], intensity_edges) - 1
-
-    # Treating the out-of-range events
-    mc_alt_bins[mc_alt_bins == len(alt_edges) - 1] = len(alt_edges) - 2
-    mc_intensity_bins[mc_intensity_bins == len(
-        intensity_edges) - 1] = len(intensity_edges) - 2
-
-    mc_weights = 1 / mc_hist[mc_alt_bins, mc_intensity_bins]
-    mc_weights *= availability_hist[mc_alt_bins, mc_intensity_bins]
-
-    # --- Storing to a data frame ---
-    mc_weight_df = pd.DataFrame(data={'event_weight': mc_weights},
-                                index=mc_data.index)
-
-    return mc_weight_df
-
-
 # =================
 # === Main code ===
 # =================
@@ -175,6 +150,7 @@ def train_direction_rf_stereo(config_file):
     }
 
     # --- Train sample ---
+    info_message("Loading train data...", prefix='DirRF')
     f_ = cfg['data_files']['mc']['train_sample']['hillas_h5']
     shower_data_train = load_dl1_data(f_)
 
