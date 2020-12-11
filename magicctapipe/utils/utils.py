df8de6f712aebc782a7ac5ad006db3379fbfb1f3
Davide Depaoli <davide.depaoli@to.infn.it>
2020-12-11 10:48:37 +0100

    Add print_elapsed_time

--- magicctapipe/utils/utils.py
+++ magicctapipe/utils/utils.py
@@ -15,3 +15,18 @@ def info_message(text, prefix='info'):
     """
     date_str = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     print(f"({prefix:s}) {date_str:s}: {text:s}")
+
+
+def print_elapsed_time(start, end):
+    """Print elapsed time as start - end. Output format is `hh:mm:ss`
+
+    Parameters
+    ----------
+    start : float
+        start time, from time.time()
+    end : float
+        stop time, from time.time()
+    """
+    h, r = divmod(end-start, 3600)
+    m, s = divmod(r, 60)
+    print(f"Elapsed time: {int(h):0>2}:{int(m):0>2}:{int(s):0>2}")
