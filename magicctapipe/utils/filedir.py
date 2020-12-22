5898319741d838a7e22c4056923526154ece21a4
Davide Depaoli <davide.depaoli@to.infn.it>
2020-12-22 11:08:07 +0100

    out_file_h5 function

--- magicctapipe/utils/filedir.py
+++ magicctapipe/utils/filedir.py
@@ -162,3 +162,28 @@ def drop_keys(df, extra_keys):
         if extra_key in df.columns:
             df.drop(extra_key, axis=1, inplace=True)
     return df
+
+
+def out_file_h5(in_file, li, hi):
+    """Returns the h5 output file name, from a simtel.gz input file
+
+    Parameters
+    ----------
+    in_file : str
+        Input file
+    li : int
+        low index
+    hi : int
+        high index
+
+    Returns
+    -------
+    str
+        h5 output file, absolute path
+    """
+    f = os.path.basename(in_file)
+    out = "_".join(f.split("_")[:li] + f.split("_")[hi:])
+    out = "%s.h5" % out.rstrip(".simtel.gz")
+    out = os.path.join(os.path.dirname(in_file), out)
+    return out
+
