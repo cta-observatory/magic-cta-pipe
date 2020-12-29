366224e016edd1945aa63067f1df93e2aefb936e
Davide Depaoli <davide.depaoli@to.infn.it>
2020-12-29 15:28:30 +0100

    fix dir error

--- train_rfs_MAGIC_LST.py
+++ train_rfs_MAGIC_LST.py
@@ -581,7 +581,7 @@ def train_energy_rf_stereo(config_file):
     cfg = load_cfg_file_check(config_file=config_file, label="energy_rf")
 
     # --- Check output directory ---
-    check_folder(cfg["classifier_rf"]["save_dir"])
+    check_folder(cfg["energy_rf"]["save_dir"])
 
     # --- Train sample ---
     f_ = cfg["data_files"]["mc"]["train_sample"]["hillas_h5"]
