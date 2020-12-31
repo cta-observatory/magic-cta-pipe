d35d84741142e5475d2be9555bb22a9846a4953a
Davide Depaoli <davide.depaoli@to.infn.it>
2020-12-31 15:22:03 +0100

    Add write_stereo and check_stereo

--- magicctapipe/reco/stereo.py
+++ magicctapipe/reco/stereo.py
@@ -93,6 +93,89 @@ def check_write_stereo(
     return stereo_params
 
 
+def check_stereo(event, tel_id, hillas_p):
+    """Check hillas parameters for stereo reconstruction
+
+    Parameters
+    ----------
+    event : ctapipe.containers.EventAndMonDataContainer
+        event
+    tel_id : int
+        telescope id
+    hillas_p : dict
+        computed hillas parameters
+
+    Returns
+    -------
+    bool
+        check_passed
+    """
+    check_passed = False
+    if len(hillas_p.keys()) > 1:
+        err_str = (
+            "Event ID %d  (obs ID: %d) has an ellipse with width = %s: "
+            "stereo parameters calculation skipped."
+        )
+        stereo_params = None
+        if any([hillas_p[tel_id]["width"].value == 0 for tel_id in hillas_p]):
+            print(err_str % (event.index.event_id, event.index.obs_id, "0"))
+        elif any([np.isnan(hillas_p[tel_id]["width"].value) for tel_id in hillas_p]):
+            print(err_str % (event.index.event_id, event.index.obs_id, "NaN"))
+        else:
+            check_passed = True
+    return check_passed
+
+
+def write_stereo(
+    stereo_id,
+    hillas_p,
+    hillas_reco,
+    subarray,
+    array_pointing,
+    telescope_pointings,
+    event_info,
+    writer,
+):
+    """Check hillas parameters and write stero parameters
+
+    Parameters
+    ----------
+    hillas_p : dict
+        computed hillas parameters
+    hillas_reco : ctapipe.reco.HillasReconstructor.HillasReconstructor
+        HillasReconstructor
+    subarray : ctapipe.instrument.subarray.SubarrayDescription
+        source.subarray
+    array_pointing : astropy.coordinates.sky_coordinate.SkyCoord
+        array_pointing
+    telescope_pointings : dict
+        telescope_pointings
+    event_info : StereoInfoContainer
+        StereoInfoContainer object
+    writer : ctapipe.io.hdf5tableio.HDF5TableWriter
+        HDF5TableWriter object
+
+    Returns
+    -------
+    ctapipe.containers.ReconstructedShowerContainer
+        stereo_params
+    """
+    # Reconstruct stereo event. From ctapipe
+    stereo_params = hillas_reco.predict(
+        hillas_dict=hillas_p,
+        subarray=subarray,
+        array_pointing=array_pointing,
+        telescopes_pointings=telescope_pointings,
+    )
+    event_info.tel_id = stereo_id
+    stereo_params.tel_ids = tel_ids_2_num(stereo_params.tel_ids)
+    # How to go back
+    # n = stereo_params.tel_ids
+    # np.where(np.array(list(bin(n)[2:][::-1]))=='1')[0]
+    writer.write("stereo_params", (event_info, stereo_params))
+    return stereo_params
+
+
 class StereoInfoContainer(Container):
     """"InfoContainer for stereo"""
 
