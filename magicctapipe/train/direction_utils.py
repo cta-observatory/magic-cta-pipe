169cdafdde5e6762a9d12ebea4ca3a2c9b19f4e8
Davide Depaoli <davide.depaoli@to.infn.it>
2021-01-02 15:06:06 +0100

    test

--- magicctapipe/train/direction_utils.py
+++ magicctapipe/train/direction_utils.py
@@ -73,7 +73,10 @@ def compute_separation_angle_direction(shower_data_test):
         s_ = separation[tel_id][~np.isnan(separation[tel_id])]
         print(f"  Tel {tel_id} scatter: ", f"{s_.to(u.deg).std():.2f}")
 
+    print(separation_df)
+
     separation_df = separation_df.dropna()
+    print(separation_df)
 
     return separation_df
 
