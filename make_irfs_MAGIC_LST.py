e2e390099afe44e82dbfbf43238c648db679fd8e
Davide Depaoli <davide.depaoli@to.infn.it>
2021-01-18 11:48:27 +0100

    add prin

--- make_irfs_MAGIC_LST.py
+++ make_irfs_MAGIC_LST.py
@@ -150,6 +150,7 @@ def make_irfs_MAGIC_LST(config_file):
             verbose=get_key_if_exists(cfg["irfs"], "verbose", False),
             eval_mean_events=True,
         )
+        log.info(f"Number of events: {len(p["events"])}")
         p["events"]["particle_type"] = particle_type
 
         p["simulated_spectrum"] = PowerLaw.from_simulation(p["simulation_info"], T_OBS)
