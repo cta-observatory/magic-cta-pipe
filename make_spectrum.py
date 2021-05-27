ad2eec64933c1ae9391d13d1985ca4d5ee6de3f6
Alessio Berti <alessioberti90@gmail.com>
2021-05-27 12:45:54 +0200

    Convert to astropy Angle.

--- make_spectrum.py
+++ make_spectrum.py
@@ -208,7 +208,7 @@ mask = exclusion_mask.geom.region_mask(exclusion_regions, inside=False)
 exclusion_mask.data = mask
 exclusion_mask.plot()
 off_positions = options["off_positions"]
-min_distance_off = 360./(off_positions+1)
+min_distance_off = Angle(360./(off_positions+1), unit="deg")
 min_distance_on  = min_distance_off
 bkg_maker = ReflectedRegionsBackgroundMaker(min_distance=min_distance_off, min_distance_input=min_distance_on, max_region_number=off_positions)
 
