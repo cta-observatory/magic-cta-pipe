e2a51fc7ed261dcb1f2987cf7a470b9c1c9f7be4
Alessio Berti <alessioberti90@gmail.com>
2021-05-28 16:19:06 +0200

    Use correct variable name.

--- hillas_preprocessing_MAGICCleaning_stereo.py
+++ hillas_preprocessing_MAGICCleaning_stereo.py
@@ -90,19 +90,19 @@ def scale_camera_geometry(camera_geom, factor):
     CameraGeometry
         Scaled camera geometry
     """
-    pix_x_scaled = factor*camera.pix_x
-    pix_y_scaled = factor*camera.pix_y
-    pix_area_scaled = camera_geom.guess_pixel_area(pix_x_scaled, pix_y_scaled, camera.pix_type)
+    pix_x_scaled = factor*camera_geom.pix_x
+    pix_y_scaled = factor*camera_geom.pix_y
+    pix_area_scaled = camera_geom.guess_pixel_area(pix_x_scaled, pix_y_scaled, camera_geom.pix_type)
 
     return CameraGeometry(
         camera_name='MAGICCam',
-        pix_id=camera.pix_id,
+        pix_id=camera_geom.pix_id,
         pix_x=pix_x_scaled,
         pix_y=pix_y_scaled,
         pix_area=pix_area_scaled,
-        pix_type=camera.pix_type,
-        pix_rotation=camera.pix_rotation,
-        cam_rotation=camera.cam_rotation
+        pix_type=camera_geom.pix_type,
+        pix_rotation=camera_geom.pix_rotation,
+        cam_rotation=camera_geom.cam_rotation
     )
 
 def process_dataset_mc(input_mask, output_name):
