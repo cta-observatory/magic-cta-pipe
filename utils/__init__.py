1ce2aa9bdea43c229aa2cff107bf0a17be4518f5
Yoshiki Ohtani <yoshiki.ohtani@cp01.ctan.cta-observatory.org>
2021-09-06 01:35:10 +0000

    put some functions to "my_function.py".

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .my_functions import calc_impact, calc_nsim
---
+++ utils/my_functions.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+def calc_impact(core_x, core_y, az, alt, tel_pos_x, tel_pos_y, tel_pos_z): 
+
+    """Calculate the Impact parameter with core positions and the Alt/Az direction of the shower axis 
+
+    Parameters
+    ----------
+    core_x : np.array, unit [m]
+        core position on the x-axis (CORSIKA coordinate, from South to North)
+    core_y : np.array, unit [m]
+        core position on the y-axis (CORSIKA coordinate, from West to East)
+    az : np.array, unit [rad]
+        azimuth direction of the shower axis
+    alt : np.array, unit [rad]
+        altitude direction of the shower axis
+    tel_pos_x : float, unit [m]
+        telescope position on the x axis (CORSIKA coordinate, from South to North)
+    tel_pos_y : float, unit [m]
+        telescope position on the y axis (CORSIKA coordinate, from West to East)
+    tel_pos_z : float, unit [m]
+        telescope position on the z axis (CORSIKA coordinate, from the reference height = 2158 [m])
+
+    Returns
+    -------
+    impact : float
+        minimum distance from the telescope position to the shower axis
+    """
+
+    t = (tel_pos_x - core_x) * np.cos(alt) * np.cos(az) - \
+        (tel_pos_y - core_y) * np.cos(alt) * np.sin(az) + \
+        tel_pos_z * np.sin(alt)    
+
+    impact = np.sqrt((core_x - tel_pos_x + t * np.cos(alt) * np.cos(az))**2 + \
+                     (core_y - tel_pos_y - t * np.cos(alt) * np.sin(az))**2 + \
+                     (t * np.sin(alt) - tel_pos_z)**2)
+    
+    return impact
+
+
+def calc_nsim(
+        n_events_sim, eslope_sim, emin_sim, emax_sim, cscat_sim, viewcone_sim, 
+        emin, emax, distmin, distmax, angmin, angmax
+): 
+
+    """Calculate the number of events simulated in the interval of energy, core, and viewcone
+
+    Parameters
+    ----------
+    nshow_sim : float
+        the number of simulated events (CORSIKA "NSHOW" parameter multiplied by CORSIKA "NSCAT" parameter and the number of runs)
+    eslope_sim : float
+        spectral index of simulations (CORSIKA "ESLOPE" parameter)
+    emin_sim : float, unit [TeV]
+        minimum energy of simulations (CORSIKA "EMIN" parameter)
+    emax_sim : float, unit [TeV]
+        maximum energy of simulations (CORSIKA "EMAX" parameter)
+    cscat_sim : float, unit [m]
+        core range of simulations (CORSIKA "CSCAT" parameter)
+    viewcone_sim : float, unit [rad]
+        viewcone of simulations (CORSIKA "VIEWCONE" parameter)
+    emin : np.array, unit [TeV]
+        minimum energy of the energy interval 
+    emax : np.array, unit [TeV]
+        maximum energy of the energy interval
+    distmin : float, unit [m]
+        minimum distance of the core interval
+    distmax : float, unit [m]
+        maximum distance of the core interval
+    angmin : float, unit [deg]
+        minimum angle of the solid angle interval
+    angmax : float, unit [deg]
+        maximum angle of the solid angle interval
+
+    Returns
+    -------
+    nsim : float
+        the number of simulated events within the interval
+    """
+
+    norm_eng = (emax**(eslope_sim+1) - emin**(eslope_sim+1))/(emax_sim**(eslope_sim+1) - emin_sim**(eslope_sim+1))
+    norm_dist = (distmax**2 - distmin**2)/cscat_sim**2
+    norm_ang = (np.cos(np.deg2rad(angmin)) - np.cos(np.deg2rad(angmax)))/(1 - np.cos(np.deg2rad(viewcone_sim)))
+
+    nsim = norm_eng * norm_dist * norm_ang * n_events_sim
+    
+    return nsim
