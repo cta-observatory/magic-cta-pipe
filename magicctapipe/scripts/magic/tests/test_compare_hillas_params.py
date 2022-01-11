2112a5c48c3adb1f1178c3c6555bb5bc9ced6f55
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-01-11 16:07:48 +0100

    use correct path to config file

--- magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
+++ magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
@@ -1,7 +1,10 @@
 from compare_hillas_params import compare_hillas_parameters
+from pathlib import Path
+
+config = Path("magicctapipe/scripts/magic/tests/compare_hillas_params.yaml").absolute()
 
 def test_compare_hillas_params():
-	list1 = compare_hillas_parameters(config_file = "compare_hillas_params.yaml", hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
+	list1 = compare_hillas_parameters(config_file = config, hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
 		test_or_train="Test", plot_image=True)
 	assert list1 == [False]*len(list1)
 	
