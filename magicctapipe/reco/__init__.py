dd412d89789a2f125ca1c2db0d4ae718c68c6adb
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-02-01 15:47:21 +0000

    Renamed the directories

--- magicctapipe/reco/__init__.py
+++ magicctapipe/reco/__init__.py
@@ -1,19 +1,66 @@
-from .image import (
-    get_num_islands_MAGIC,
-    clean_image_params,
-    eval_impact,
-    tailcuts_clean_lstchain,
+from .classifier_utils import (
+    GetHist_classifier,
+    evaluate_performance_classifier,
+    get_weights_classifier,
+    print_par_imp_classifier,
+    load_init_data_classifier,
+    check_train_test_intersections_classifier,
     )
 
-from .stereo import (
-    write_hillas,
-    check_write_stereo,
-    check_stereo,
-    write_stereo,
+from .direction_utils import (
+    compute_separation_angle_direction,
     )
 
-__all__ = ["get_num_islands_MAGIC", "clean_image_params",
-           "eval_impact", "tailcuts_clean_lstchain",
-           "write_hillas", "check_write_stereo",
-           "check_stereo", "write_stereo"]
+from .energy_utils import (
+    GetHist2D_energy,
+    evaluate_performance_energy,
+    plot_migmatrix,
+    )
+
+from .event_processing import (
+    RegressorClassifierBase,
+    EnergyRegressor,
+    HillasFeatureSelector,
+    EventFeatureSelector,
+    EventFeatureTargetSelector,
+    EventProcessor,
+    EnergyEstimator,
+    EnergyEstimatorPandas,
+    DirectionEstimatorPandas,
+    EventClassifierPandas,
+    DirectionStereoEstimatorPandas,
+    )
+
+from .global_utils import (
+    compute_event_weights,
+    get_weights_mc_dir_class,
+    check_train_test_intersections,
+    )
+
+__all__ = [
+    "GetHist_classifier",
+    "evaluate_performance_classifier",
+    "get_weights_classifier",
+    "print_par_imp_classifier",
+    "load_init_data_classifier",
+    "check_train_test_intersections_classifier",
+    "compute_separation_angle_direction",
+    "GetHist2D_energy",
+    "evaluate_performance_energy",
+    "plot_migmatrix",
+    "RegressorClassifierBase",
+    "EnergyRegressor",
+    "HillasFeatureSelector",
+    "EventFeatureSelector",
+    "EventFeatureTargetSelector",
+    "EventProcessor",
+    "EnergyEstimator",
+    "EnergyEstimatorPandas",
+    "DirectionEstimatorPandas",
+    "EventClassifierPandas",
+    "DirectionStereoEstimatorPandas",
+    "compute_event_weights",
+    "get_weights_mc_dir_class",
+    "check_train_test_intersections",
+]
 
