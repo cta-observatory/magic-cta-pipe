37c165107d7b03333922ce0c8120ae0378bfe2c6
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-02-01 16:18:50 +0000

    Moved leakage.py from utils to image directory, and modified __init__.py

--- magicctapipe/image/__init__.py
+++ magicctapipe/image/__init__.py
@@ -8,6 +8,10 @@ from .cleaning import (
     tailcuts_clean_lstchain,
 )
 
+from .leakage import (
+    get_leakage,
+)
+
 from .modifier import (
     add_noise_in_pixels,
     set_numba_seed,
@@ -29,6 +33,7 @@ __all__ = [
     'clean_image_params',
     'eval_impact',
     'tailcuts_clean_lstchain',
+    'get_leakage',
     'add_noise_in_pixels',
     'set_numba_seed',
     'random_psf_smearer'
