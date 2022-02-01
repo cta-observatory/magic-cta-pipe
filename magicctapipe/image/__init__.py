56b25318a2d62a00346c8a68625f6d67d6b8a859
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-02-01 16:01:49 +0000

    Modify image/__init__.py again

--- magicctapipe/image/__init__.py
+++ magicctapipe/image/__init__.py
@@ -8,6 +8,12 @@ from .cleaning import (
     tailcuts_clean_lstchain,
 )
 
+from .modifier import (
+    add_noise_in_pixels,
+    set_numba_seed,
+    random_psf_smearer,
+)
+
 from .stereo import (
     write_hillas,
     check_write_stereo,
@@ -23,6 +29,9 @@ __all__ = [
     'clean_image_params',
     'eval_impact',
     'tailcuts_clean_lstchain',
+    'add_noise_in_pixels',
+    'set_numba_seed',
+    'random_psf_smearer'
     'write_hillas',
     'check_write_stereo',
     'check_stereo',
