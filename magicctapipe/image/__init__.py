5ea1feabc514ead5a25826ae69b7a6e95ec3fade
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-02-01 15:57:08 +0000

    Modified image/__init__.py

--- magicctapipe/image/__init__.py
+++ magicctapipe/image/__init__.py
@@ -1,19 +1,31 @@
-from .image import (
+from .cleaning import (
+    MAGICClean,
+    PixelTreatment,
+    apply_dynamic_cleaning,
     get_num_islands_MAGIC,
     clean_image_params,
     eval_impact,
     tailcuts_clean_lstchain,
-    )
+)
 
 from .stereo import (
     write_hillas,
     check_write_stereo,
     check_stereo,
     write_stereo,
-    )
+)
 
-__all__ = ["get_num_islands_MAGIC", "clean_image_params",
-           "eval_impact", "tailcuts_clean_lstchain",
-           "write_hillas", "check_write_stereo",
-           "check_stereo", "write_stereo"]
+__all__ = [
+    'MAGICClean',
+    'PixelTreatment',
+    'apply_dynamic_cleaning',
+    'get_num_islands_MAGIC',
+    'clean_image_params',
+    'eval_impact',
+    'tailcuts_clean_lstchain',
+    'write_hillas',
+    'check_write_stereo',
+    'check_stereo',
+    'write_stereo',
+]
 
