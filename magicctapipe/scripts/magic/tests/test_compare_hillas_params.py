4c1c951ab87a4f6077b2a20c90bcc4fb0fa75b0d
Alessio Berti <alessioberti90@gmail.com>
2022-02-04 21:52:31 +0100

    Convert indentation to spaces.

--- magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
+++ magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
@@ -4,7 +4,6 @@ from pathlib import Path
 config = Path("magicctapipe/scripts/magic/tests/compare_hillas_params.yaml").absolute()
 
 def test_compare_hillas_params():
-	list1 = compare_hillas_parameters(config_file = config, hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
-		test_or_train="Test", plot_image=True)
-	assert list1 == [False]*len(list1)
-	
+    list1 = compare_hillas_parameters(config_file = config, hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data",
+        test_or_train="Test", plot_image=True)
+    assert list1 == [False]*len(list1)
