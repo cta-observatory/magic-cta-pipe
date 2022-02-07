a6f9f59b53b346daa0398d5e72260ff82bc7e17e
Alessio Berti <alessioberti90@gmail.com>
2022-02-07 15:48:16 +0100

    Add loop for event comparison.

--- magicctapipe/scripts/comparison/compare_dl1_images.py
+++ magicctapipe/scripts/comparison/compare_dl1_images.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import numpy as np
 
 from ctapipe.io import EventSource
 
@@ -20,28 +21,44 @@ def compare_dl1_images(input_file_1, input_file_2):
 
     event_ids_1 = []
     images_1 = []
-    clean_mask_1 = []
+    clean_masks_1 = []
 
     source_1 = EventSource(input_url=input_file_1)
 
     for event in source_1:
         event_ids_1.append(event.index.event_id)
         images_1.append(event.dl1.tel[1].image)
-        clean_mask_1.append(event.dl1.tel[1].image_mask)
+        clean_masks_1.append(event.dl1.tel[1].image_mask)
 
     event_ids_2 = []
     images_2 = []
-    clean_mask_2 = []
+    clean_masks_2 = []
 
     source_2 = EventSource(input_url=input_file_2)
 
     for event in source_2:
         event_ids_2.append(event.index.event_id)
         images_2.append(event.dl1.tel[1].image)
-        clean_mask_2.append(event.dl1.tel[1].image_mask)
+        clean_masks_2.append(event.dl1.tel[1].image_mask)
 
     if event_ids_1 != event_ids_2:
         print("The two files have different event ids.")
+
+    for event_i in range(len(event_ids_1)):
+        image_1 = np.array(images_1[event_i])
+        image_2 = np.array(images_2[event_i])
+        image_diff = np.absolute(image_1 - image_2)
+        diff_gtr_0 = np.where(image_diff > 0.01)[0]
+        if len(diff_gtr_0) > 0:
+            print(f"Event ID {event_ids_1[event_i]} has different images.")
+            print(image_diff[diff_gtr_0])
+
+        clean_mask_1 = clean_masks_1[event_i]
+        clean_mask_2 = clean_masks_2[event_i]
+        if not np.array_equal(clean_mask_1, clean_mask_2):
+            print(f"Event ID {event_ids_1[event_i]} has different cleaning masks.")
+            clean_mask_diff = clean_mask_1 == clean_mask_2
+            print(np.where(clean_mask_diff is False)[0][0])
 
 
 def main(*args):
