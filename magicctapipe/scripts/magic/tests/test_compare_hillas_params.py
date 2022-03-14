739800e54fffaa16ebd292e35a90441933de541b
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-03-14 10:58:12 +0100

    Updated test for hillas params comparison

--- magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
+++ magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
@@ -1,10 +1,10 @@
 from compare_hillas_params import compare_hillas_parameters
 from pathlib import Path
 
-config = Path("magicctapipe/scripts/magic/tests/compare_hillas_params.yaml").absolute()
+config = Path("compare_hillas_config.yaml").absolute()
 
 def test_compare_hillas_params():
-	list1 = compare_hillas_parameters(config_file = config, hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
+	list1 = compare_hillas_parameters(config_file = str(config), hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
 		test_or_train="Test", plot_image=True)
 	assert list1 == [False]*len(list1)
 	
