7dcfdbd4fee686f4a3b716b0ef7dbc2c2cc8e001
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-03-16 12:09:06 +0100

    Test image comparison for both telescopes

--- magicctapipe/scripts/magic/tests/test_image_comparison.py
+++ magicctapipe/scripts/magic/tests/test_image_comparison.py
@@ -4,5 +4,6 @@ from pathlib import Path
 config=Path("image_comparison_config.yaml").absolute()
 
 def test_image_comparison():
-	list1 = image_comparison(config_file = str(config), mode = "use_ids_config", tel_id=2)
-	assert list1 == []
+	list1 = image_comparison(config_file = str(config), mode = "use_ids_config", tel_id=1)
+	list2 = image_comparison(config_file = str(config), mode = "use_ids_config", tel_id=2)
+	assert (list1, list2) == ([], [])
