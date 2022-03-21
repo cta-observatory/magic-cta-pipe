19d74782ac56c1f64a5b5dee472ed8f9a0e75e03
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-03-21 13:36:32 +0100

    Removed unnecessary arguments of function

--- magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
+++ magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
@@ -4,7 +4,5 @@ from pathlib import Path
 config = Path("compare_hillas_config.yaml").absolute()
 
 def test_compare_hillas_params():
-	list1 = compare_hillas_parameters(config_file = str(config), hillas_key="dl1/hillas_params", subrun=5086952, data_or_mc="Data", 
-		test_or_train="Test", plot_image=True)
+	list1 = compare_hillas_parameters(config_file = str(config), hillas_key="dl1/hillas_params", subrun=5086952, plot_image=True)
 	assert list1 == [False]*len(list1)
-	
