0eeb563101db764c978f7382ef1a67e4cd5add53
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-04-27 05:10:04 +0000

    Removed the imports of the functions that are not used in the main functions

--- magicctapipe/scripts/lst1_magic/__init__.py
+++ magicctapipe/scripts/lst1_magic/__init__.py
@@ -1,10 +1,7 @@
+from .create_dl3_index_files import create_dl3_index_files
+from .lst1_magic_create_irf import create_irf
 from .lst1_magic_dl1_stereo_to_dl2 import dl1_stereo_to_dl2
-from .lst1_magic_dl2_to_dl3 import (
-    create_event_list,
-    create_gti_table,
-    create_pointing_table,
-    dl2_to_dl3,
-)
+from .lst1_magic_dl2_to_dl3 import dl2_to_dl3
 from .lst1_magic_event_coincidence import event_coincidence
 from .lst1_magic_mc_dl0_to_dl1 import mc_dl0_to_dl1
 from .lst1_magic_stereo_reco import stereo_reconstruction
@@ -13,21 +10,13 @@ from .lst1_magic_train_rfs import (
     train_direction_regressor,
     train_event_classifier,
 )
-from .lst1_magic_create_irf import (
-    load_dl2_data_file,
-    apply_dynamic_gammaness_cut,
-    apply_dynamic_theta_cut,
-    create_irf,
-)
-from .create_dl3_index_files import create_dl3_index_files
 from .magic_calib_to_dl1 import magic_calib_to_dl1
 from .merge_hdf_files import merge_hdf_files
 
 __all__ = [
+    'create_dl3_index_files',
+    'create_irf',
     'dl1_stereo_to_dl2',
-    'create_event_list',
-    'create_gti_table',
-    'create_pointing_table',
     'dl2_to_dl3',
     'event_coincidence',
     'mc_dl0_to_dl1',
@@ -35,11 +24,6 @@ __all__ = [
     'train_energy_regressor',
     'train_direction_regressor',
     'train_event_classifier',
-    'create_dl3_index_files',
-    'load_dl2_data_file',
-    'apply_dynamic_gammaness_cut',
-    'apply_dynamic_theta_cut',
-    'create_irf',
     'magic_calib_to_dl1',
     'merge_hdf_files',
 ]
