a4551a455504110ff677bad9409b6d8486fc66d8
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-05-02 15:37:29 +0200

    Adapted key for .h5 file-format

--- magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
+++ magicctapipe/scripts/magic/tests/test_compare_hillas_params.py
@@ -4,5 +4,5 @@ from pathlib import Path
 config = Path("compare_hillas_config.yaml").absolute()
 
 def test_compare_hillas_params():
-	list1 = compare_hillas_parameters(config_file = str(config), hillas_key="dl1/hillas_params", subrun=5086952, plot_image=True)
+	list1 = compare_hillas_parameters(config_file = str(config), hillas_key="events/parameters", subrun=5093711, plot_image=True)
 	assert list1 == [False]*len(list1)
