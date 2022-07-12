48424fa4db125450cd9b261fa221458ba453a34d
Alessio Berti <alessioberti90@gmail.com>
2022-07-12 14:05:14 +0200

    Put image and superstar files in different output directories.

--- download_data.sh
+++ download_data.sh
@@ -6,11 +6,13 @@ echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/cal
 echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/calibrated/20210314_M1_05095172.002_Y_CrabNebula-W0.40+035.root" >> test_data_real.txt
 echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/calibrated/20210314_M2_05095172.001_Y_CrabNebula-W0.40+035.root" >> test_data_real.txt
 echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/calibrated/20210314_M2_05095172.002_Y_CrabNebula-W0.40+035.root" >> test_data_real.txt
-echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M1_05095172.001_I_CrabNebula-W0.40+035.h5" >  test_data_real.txt
-echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M1_05095172.002_I_CrabNebula-W0.40+035.h5" >> test_data_real.txt
-echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M2_05095172.001_I_CrabNebula-W0.40+035.h5" >> test_data_real.txt
-echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M2_05095172.002_I_CrabNebula-W0.40+035.h5" >> test_data_real.txt
-echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/superstar/20210314_05095172_S_CrabNebula-W0.40+035.root" >> test_data_real.txt
+
+echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M1_05095172.001_I_CrabNebula-W0.40+035.h5" >  test_data_images_real.txt
+echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M1_05095172.002_I_CrabNebula-W0.40+035.h5" >> test_data_images_real.txt
+echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M2_05095172.001_I_CrabNebula-W0.40+035.h5" >> test_data_images_real.txt
+echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/images/20210314_M2_05095172.002_I_CrabNebula-W0.40+035.h5" >> test_data_images_real.txt
+
+echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/real/superstar/20210314_05095172_S_CrabNebula-W0.40+035.root" > test_data_superstar_real.txt
 
 echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/simulated/calibrated/GA_M1_za35to50_8_824318_Y_w0.root" >  test_data_simulated.txt
 echo "https://webdav-magic.pic.es:8451/Users/ctapipe_io_magic/test_data/simulated/calibrated/GA_M1_za35to50_8_824319_Y_w0.root" >> test_data_simulated.txt
@@ -41,6 +43,28 @@ if ! wget \
 fi
 
 if ! wget \
+    -i test_data_images_real.txt \
+    --user="$TEST_DATA_USER" \
+    --password="$TEST_DATA_PASSWORD" \
+    --no-check-certificate \
+    --no-verbose \
+    --timestamping \
+    --directory-prefix=test_data/real/images; then
+    echo "Problem in downloading the test data set for real data."
+fi
+
+if ! wget \
+    -i test_data_superstar_real.txt \
+    --user="$TEST_DATA_USER" \
+    --password="$TEST_DATA_PASSWORD" \
+    --no-check-certificate \
+    --no-verbose \
+    --timestamping \
+    --directory-prefix=test_data/real/superstar; then
+    echo "Problem in downloading the test data set for real data."
+fi
+
+if ! wget \
     -i test_data_simulated.txt \
     --user="$TEST_DATA_USER" \
     --password="$TEST_DATA_PASSWORD" \
@@ -51,4 +75,4 @@ if ! wget \
     echo "Problem in downloading the test data set for simulated data."
 fi
 
-rm -f test_data_real.txt test_data_simulated.txt
+rm -f test_data_real.txt test_data_simulated.txt test_data_images_real.txt test_data_superstar_real.txt
