16f321d86cb6603b5710088959236191111697b4
Alessio Berti <alessioberti90@gmail.com>
2022-07-12 17:28:18 +0200

    Reformat and add output directories.

--- magicctapipe/scripts/mars/tests/test_compare_params.py
+++ magicctapipe/scripts/mars/tests/test_compare_params.py
@@ -1,58 +1,150 @@
 import os
 from compare_hillas_stereo_params import compare_hillas_stereo_parameters
 from pathlib import Path
-from pkg_resources import resource_filename
 import yaml
 
 import pytest
 
-test_data = Path(os.getenv('MAGIC_TEST_DATA', 'test_data')).absolute()
-test_calibrated_real_dir = test_data / 'real/calibrated'
-test_superstar_real_dir = test_data / 'real/superstar'
+test_data = Path(os.getenv("MAGIC_TEST_DATA", "test_data")).absolute()
+test_calibrated_real_dir = test_data / "real/calibrated"
+test_superstar_real_dir = test_data / "real/superstar"
 
 test_calibrated_mars_M1_real = [
-    test_calibrated_real_dir / '20210314_M1_05095172.001_Y_CrabNebula-W0.40+035.root',
+    test_calibrated_real_dir / "20210314_M1_05095172.001_Y_CrabNebula-W0.40+035.root",
 ]
 
 test_calibrated_mars_M2_real = [
-    test_calibrated_real_dir / '20210314_M2_05095172.001_Y_CrabNebula-W0.40+035.root',
+    test_calibrated_real_dir / "20210314_M2_05095172.001_Y_CrabNebula-W0.40+035.root",
 ]
 
-# TODO: produce these files
 test_superstar_mars_real = [
-    test_superstar_real_dir / '20210314_05095172_S_CrabNebula-W0.40+035.root',
+    test_superstar_real_dir / "20210314_05095172_S_CrabNebula-W0.40+035.root",
 ]
 
-config = resource_filename(
-    'magicctapipe', 'scripts/mars/tests/compare_params_config.yaml'
+
+@pytest.mark.parametrize(
+    "dataset_calibrated_M1, dataset_calibrated_M2, dataset_superstar",
+    [
+        (
+            test_calibrated_mars_M1_real[0],
+            test_calibrated_mars_M2_real[0],
+            test_superstar_mars_real[0],
+        )
+    ],
 )
+def test_compare_hillas_stereo_params(
+    dataset_calibrated_M1, dataset_calibrated_M2, dataset_superstar, tmp_path
+):
+    from magicctapipe.scripts.lst1_magic import (
+        magic_calib_to_dl1,
+        merge_hdf_files,
+        stereo_reconstruction,
+    )
 
-config_mcp_file = resource_filename(
-    'magicctapipe', 'scripts/lst1_magic/config.yaml'
-)
+    params_list = [
+        "width_M1",
+        "width_M2",
+        "length_M1",
+        "length_M2",
+        "size_M1",
+        "size_M2",
+        "delta_M1",
+        "delta_M2",
+        "cogx_M1",
+        "cogx_M2",
+        "cogy_M1",
+        "cogy_M2",
+        "corex",
+        "corey",
+        "az",
+        "zd",
+        "hmax",
+        "impact_M1",
+        "impact_M2",
+        "slope_M1",
+        "slope_M2",
+    ]
 
+    config_params = {
+        "magic-cta-pipe-input": {
+            "MCP-path": str(
+                test_data / "real/dl1_stereo" / "dl1_stereo_MAGIC.Run05095172.h5"
+            )
+        },
+        "MARS-input": {"MARS-path": str(dataset_superstar)},
+        "Params": params_list,
+        "Output_paths": {
+            "file_output_directory": str(test_data / "real/test_params"),
+            "image_output_directory": str(test_data / "real/test_params"),
+        },
+    }
 
-# TODO: check if this will consider all combinations or just one to one
-@pytest.mark.parametrize('dataset_calibrated_M1, dataset_calibrated_M2, dataset_superstar', [(test_calibrated_mars_M1_real[0], test_calibrated_mars_M2_real[0], test_calibrated_mars_M2_real)])
-def test_compare_hillas_stereo_params(dataset_calibrated_M1, dataset_calibrated_M2, dataset_superstar):
-    from magicctapipe.scripts.lst1_magic import magic_calib_to_dl1, merge_hdf_files, stereo_reconstruction
+    config_params_file = str(tmp_path / "compare_params_config.yaml")
 
-    with yaml.safe_load(open(config, "rw")) as config_file:
-        config_file["MARS-input"]["MARS-path"] = dataset_superstar
+    with open(config_params_file, "w") as outfile:
+        yaml.dump(config_params, outfile, default_flow_style=False)
 
-    with open(config_mcp_file, 'rb') as f:
+    config_mcp = {
+        "mc_tel_ids": {"LST-1": 1, "MAGIC-I": 2, "MAGIC-II": 3},
+        "MAGIC": {
+            "image_extractor": {
+                "type": "SlidingWindowMaxSum",
+                "window_width": 5,
+                "apply_integration_correction": False,
+            },
+            "charge_correction": {"use": True, "correction_factor": 1.143},
+            "magic_clean": {
+                "use_time": True,
+                "use_sum": True,
+                "picture_thresh": 6,
+                "boundary_thresh": 3.5,
+                "max_time_off": 4.5,
+                "max_time_diff": 1.5,
+                "find_hotpixels": "auto",
+                "pedestal_type": "from_extractor_rndm",
+            },  # select 'fundamental', 'from_extractor' or 'from_extractor_rndm'
+            "muon_ring": {
+                "thr_low": 25,
+                "tailcut": [12, 8],
+                "ring_completeness_threshold": 25,
+            },
+        },
+        "event_coincidence": {
+            "type_lst_time": "dragon_time",  # select 'dragon_time', 'tib_time' or 'ucts_time'
+            "window_width": 0.6,  # unit: [usec]
+            "offset_start": -10.0,  # unit: [usec]
+            "offset_stop": 0.0,
+        },  # unit: [usec]
+        "stereo_reco": {"quality_cuts": "(intensity > 50) & (width > 0)"},
+    }
+
+    config_mcp_file = str(tmp_path / "config_mcp.yaml")
+
+    with open(config_mcp_file, "w") as outfile:
+        yaml.dump(config_mcp, outfile, default_flow_style=False)
+
+    with open(config_mcp_file, "rb") as f:
         config_mcp = yaml.safe_load(f)
 
-    magic_calib_to_dl1(dataset_calibrated_M1, test_data / 'real/dl1', config_mcp, True)
-    magic_calib_to_dl1(dataset_calibrated_M2, test_data / 'real/dl1', config_mcp, True)
+    magic_calib_to_dl1(dataset_calibrated_M1, test_data / "real/dl1", config_mcp, True)
+    magic_calib_to_dl1(dataset_calibrated_M2, test_data / "real/dl1", config_mcp, True)
 
-    merge_hdf_files(test_data / 'real/dl1', output_dir=test_data / 'real/dl1_merged', run_wise=True, subrun_wise=False)
+    merge_hdf_files(
+        test_data / "real/dl1",
+        output_dir=test_data / "real/dl1_merged",
+        run_wise=True,
+        subrun_wise=False,
+    )
 
-    stereo_reconstruction(test_data / 'real/dl1_merged' / 'dl1_MAGIC.Run05095172.h5', test_data / 'real/dl1_stereo' , config_mcp, magic_only=True)
+    stereo_reconstruction(
+        test_data / "real/dl1_merged" / "dl1_MAGIC.Run05095172.h5",
+        test_data / "real/dl1_stereo",
+        config_mcp,
+        magic_only=True,
+    )
 
-    with yaml.safe_load(open(config, "rw")) as config_file:
-        config_file["magic-cta-pipe-input"]["MCP-path"] = test_data / 'real/dl1_stereo' / 'dl1_stereo_MAGIC.Run05095172.h5'
-        config_file["MARS-input"]["MARS-path"] = dataset_superstar
-
-    list_compare_parameters = compare_hillas_stereo_parameters(config_file=config, params_key="events/parameters", plot_image=True)
+    list_compare_parameters = compare_hillas_stereo_parameters(
+        config_file=config_params_file, params_key="events/parameters", plot_image=True
+    )
+    print(list_compare_parameters)
     assert list_compare_parameters == [False] * len(list_compare_parameters)
