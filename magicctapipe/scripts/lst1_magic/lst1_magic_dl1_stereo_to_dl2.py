265a73e97f6e55050544c889331656e40e94bbae
YoshikiOhtani <ohtani@icrr.u-tokyo.ac.jp>
2022-07-25 11:21:03 +0900

    Modify dl1_stereo_to_dl2, follow suggestions by flake8, run isort and black

--- magicctapipe/scripts/lst1_magic/lst1_magic_dl1_stereo_to_dl2.py
+++ magicctapipe/scripts/lst1_magic/lst1_magic_dl1_stereo_to_dl2.py
@@ -13,37 +13,33 @@ $ python lst1_magic_dl1_stereo_to_dl2.py
 --output-dir ./data/dl2
 """
 
-import re
-import glob
-import time
-import logging
 import argparse
-import numpy as np
-import pandas as pd
+import glob
+import logging
+import re
+import time
 from pathlib import Path
+
+import pandas as pd
 from astropy import units as u
 from astropy.time import Time
 from ctapipe.instrument import SubarrayDescription
-from magicctapipe.reco import (
-    EnergyRegressor,
-    DirectionRegressor,
-    EventClassifier,
-)
+from magicctapipe.reco import DirectionRegressor, EnergyRegressor, EventClassifier
 from magicctapipe.utils import (
     check_tel_combination,
-    transform_altaz_to_radec,
     save_pandas_to_table,
+    transform_altaz_to_radec,
 )
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
-nsec2sec = 1e-9
+NSEC2SEC = 1e-9
 
 __all__ = [
-    'apply_rfs',
-    'dl1_stereo_to_dl2',
+    "apply_rfs",
+    "dl1_stereo_to_dl2",
 ]
 
 
@@ -66,26 +62,22 @@ def apply_rfs(event_data, estimator):
 
     tel_ids = list(estimator.telescope_rfs.keys())
 
-    df_events = event_data.query(f'(tel_id == {tel_ids}) & (multiplicity == {len(tel_ids)})').copy()
-    df_events['multiplicity'] = df_events.groupby(['obs_id', 'event_id']).size()
-    df_events.query(f'multiplicity == {len(tel_ids)}', inplace=True)
+    df_events = event_data.query(
+        f"(tel_id == {tel_ids}) & (multiplicity == {len(tel_ids)})"
+    ).copy()
 
-    n_events = len(df_events.groupby(['obs_id', 'event_id']).size())
+    df_events["multiplicity"] = df_events.groupby(["obs_id", "event_id"]).size()
+    df_events.query(f"multiplicity == {len(tel_ids)}", inplace=True)
 
-    if n_events > 0:
-        logger.info(f'--> {n_events} events are found. Applying...')
-        reco_params = estimator.predict(df_events)
-    else:
-        logger.warning('--> No corresponding events are found. Skipping.')
-        reco_params = pd.DataFrame()
+    reco_params = estimator.predict(df_events)
 
     return reco_params
 
 
 def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
     """
-    Processes DL1-stereo events and
-    reconstructs the DL2 parameters with trained RFs.
+    Processes DL1-stereo events and reconstructs
+    the DL2 parameters with trained RFs.
 
     Parameters
     ----------
@@ -97,25 +89,25 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
         Path to a directory where to save an output DL2 data file
     """
 
-    logger.info('\nLoading the input DL1-stereo data file:')
-    logger.info(input_file_dl1)
+    logger.info(f"\nInput DL1-stereo data file:\n{input_file_dl1}")
+    logger.info(f"\nInput RF directory:\n{input_dir_rfs}")
 
-    event_data = pd.read_hdf(input_file_dl1, key='events/parameters')
-    event_data.set_index(['obs_id', 'event_id', 'tel_id'], inplace=True)
+    event_data = pd.read_hdf(input_file_dl1, key="events/parameters")
+    event_data.set_index(["obs_id", "event_id", "tel_id"], inplace=True)
     event_data.sort_index(inplace=True)
 
     check_tel_combination(event_data)
 
-    is_simulation = ('true_energy' in event_data.columns)
+    is_simulation = "true_energy" in event_data.columns
 
     # Reconstruct energy:
-    mask_energy_rf = f'{input_dir_rfs}/energy_regressor*.joblib'
+    mask_energy_rf = f"{input_dir_rfs}/energy_regressor*.joblib"
 
     input_rfs_energy = glob.glob(mask_energy_rf)
     input_rfs_energy.sort()
 
     if len(input_rfs_energy) > 0:
-        logger.info('\nReconstructing energy...')
+        logger.info("\nReconstructing energy...")
 
         reco_params = pd.DataFrame()
         energy_regressor = EnergyRegressor()
@@ -123,10 +115,9 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
         for input_rfs in input_rfs_energy:
 
             logger.info(input_rfs)
-
             energy_regressor.load(input_rfs)
-            df_reco_energy = apply_rfs(event_data, energy_regressor)
 
+            df_reco_energy = apply_rfs(event_data, energy_regressor)
             reco_params = reco_params.append(df_reco_energy)
 
         event_data = event_data.join(reco_params)
@@ -134,13 +125,13 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
         del energy_regressor
 
     # Reconstruct arrival directions:
-    mask_direction_rf = f'{input_dir_rfs}/direction_regressor*.joblib'
+    mask_direction_rf = f"{input_dir_rfs}/direction_regressor*.joblib"
 
     input_rfs_direction = glob.glob(mask_direction_rf)
     input_rfs_direction.sort()
 
     if len(input_rfs_direction) > 0:
-        logger.info('\nReconstructing arrival directions...')
+        logger.info("\nReconstructing arrival directions...")
 
         reco_params = pd.DataFrame()
         direction_regressor = DirectionRegressor()
@@ -148,56 +139,61 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
         for input_rfs in input_rfs_direction:
 
             logger.info(input_rfs)
-
             direction_regressor.load(input_rfs)
-            df_reco_direction = apply_rfs(event_data, direction_regressor)
 
+            df_reco_direction = apply_rfs(event_data, direction_regressor)
             reco_params = reco_params.append(df_reco_direction)
 
         event_data = event_data.join(reco_params)
 
         if not is_simulation:
-            logger.info('\nTransforming the Alt/Az coordinate to the RA/Dec coordinate...')
+            logger.info(
+                "\nTransforming the Alt/Az coordinate to the RA/Dec coordinate..."
+            )
 
-            if 'timestamp' in event_data.columns:
-                timestamps = Time(event_data['timestamp'].to_numpy(), format='unix', scale='utc')
+            if "timestamp" in event_data.columns:
+                timestamps = Time(
+                    event_data["timestamp"].to_numpy(),
+                    format="unix",
+                    scale="utc",
+                )
 
             else:
-                time_sec = event_data['time_sec'].to_numpy()
-                time_nanosec = event_data['time_nanosec'].to_numpy() * nsec2sec
+                time_sec = event_data["time_sec"].to_numpy()
+                time_nanosec = event_data["time_nanosec"].to_numpy() * NSEC2SEC
 
-                timestamps = Time(time_sec + time_nanosec, format='unix', scale='utc')
+                timestamps = Time(time_sec + time_nanosec, format="unix", scale="utc")
 
-                event_data['timestamp'] = timestamps.value
-                event_data.drop(columns=['time_sec', 'time_nanosec'], inplace=True)
+                event_data["timestamp"] = timestamps.value
+                event_data.drop(columns=["time_sec", "time_nanosec"], inplace=True)
 
             pointing_ra, pointing_dec = transform_altaz_to_radec(
-                alt=u.Quantity(event_data['pointing_alt'].values, u.rad),
-                az=u.Quantity(event_data['pointing_az'].values, u.rad),
+                alt=u.Quantity(event_data["pointing_alt"].values, u.rad),
+                az=u.Quantity(event_data["pointing_az"].values, u.rad),
                 timestamp=timestamps,
             )
 
             reco_ra, reco_dec = transform_altaz_to_radec(
-                alt=u.Quantity(event_data['reco_alt'].values, u.deg),
-                az=u.Quantity(event_data['reco_az'].values, u.deg),
+                alt=u.Quantity(event_data["reco_alt"].values, u.deg),
+                az=u.Quantity(event_data["reco_az"].values, u.deg),
                 timestamp=timestamps,
             )
 
-            event_data['pointing_ra'] = pointing_ra.to(u.deg).value
-            event_data['pointing_dec'] = pointing_dec.to(u.deg).value
-            event_data['reco_ra'] = reco_ra.to(u.deg).value
-            event_data['reco_dec'] = reco_dec.to(u.deg).value
+            event_data["pointing_ra"] = pointing_ra.to(u.deg).value
+            event_data["pointing_dec"] = pointing_dec.to(u.deg).value
+            event_data["reco_ra"] = reco_ra.to(u.deg).value
+            event_data["reco_dec"] = reco_dec.to(u.deg).value
 
         del direction_regressor
 
     # Reconstruct the gammaness:
-    mask_classifier = f'{input_dir_rfs}/event_classifier*.joblib'
+    mask_classifier = f"{input_dir_rfs}/event_classifier*.joblib"
 
     input_rfs_classifier = glob.glob(mask_classifier)
     input_rfs_classifier.sort()
 
     if len(input_rfs_classifier) > 0:
-        logger.info('\nReconstructing the gammaness...')
+        logger.info("\nReconstructing the gammaness...")
 
         reco_params = pd.DataFrame()
         event_classifier = EventClassifier()
@@ -205,10 +201,9 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
         for input_rfs in input_rfs_classifier:
 
             logger.info(input_rfs)
-
             event_classifier.load(input_rfs)
-            df_reco_class = apply_rfs(event_data, event_classifier)
 
+            df_reco_class = apply_rfs(event_data, event_classifier)
             reco_params = reco_params.append(df_reco_class)
 
         event_data = event_data.join(reco_params)
@@ -218,27 +213,40 @@ def dl1_stereo_to_dl2(input_file_dl1, input_dir_rfs, output_dir):
     # Save the data in an output file:
     Path(output_dir).mkdir(exist_ok=True, parents=True)
 
-    regex = r'dl1_stereo_(\S+)\.h5'
+    regex = r"dl1_stereo_(\S+)\.h5"
     file_name = Path(input_file_dl1).name
 
     if re.fullmatch(regex, file_name):
         parser = re.findall(regex, file_name)[0]
-        output_file = f'{output_dir}/dl2_{parser}.h5'
+        output_file = f"{output_dir}/dl2_{parser}.h5"
     else:
-        raise RuntimeError('Could not parse information from the input file name.')
+        raise RuntimeError("Could not parse information from the input file name.")
 
     event_data.reset_index(inplace=True)
-    save_pandas_to_table(event_data, output_file, group_name='/events', table_name='parameters', mode='w')
+
+    save_pandas_to_table(
+        event_data,
+        output_file,
+        group_name="/events",
+        table_name="parameters",
+        mode="w",
+    )
+
+    if is_simulation:
+        sim_config = pd.read_hdf(input_file_dl1, key="simulation/config")
+
+        save_pandas_to_table(
+            sim_config,
+            output_file,
+            group_name="/simulation",
+            table_name="config",
+            mode="a",
+        )
 
     subarray = SubarrayDescription.from_hdf(input_file_dl1)
     subarray.to_hdf(output_file)
 
-    if is_simulation:
-        sim_config = pd.read_hdf(input_file_dl1, key='simulation/config')
-        save_pandas_to_table(sim_config, output_file, group_name='/simulation', table_name='config', mode='a')
-
-    logger.info('\nOutput file:')
-    logger.info(output_file)
+    logger.info(f"\nOutput file:\n{output_file}")
 
 
 def main():
@@ -248,18 +256,30 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--input-file-dl1', '-d', dest='input_file_dl1', type=str, required=True,
-        help='Path to an input DL1-stereo data file.',
+        "--input-file-dl1",
+        "-d",
+        dest="input_file_dl1",
+        type=str,
+        required=True,
+        help="Path to an input DL1-stereo data file.",
     )
 
     parser.add_argument(
-        '--input-dir-rfs', '-r', dest='input_dir_rfs', type=str, required=True,
-        help='Path to a directory where trained RFs are stored.',
+        "--input-dir-rfs",
+        "-r",
+        dest="input_dir_rfs",
+        type=str,
+        required=True,
+        help="Path to a directory where trained RFs are stored.",
     )
 
     parser.add_argument(
-        '--output-dir', '-o', dest='output_dir', type=str, default='./data',
-        help='Path to a directory where to save an output DL2 data file.',
+        "--output-dir",
+        "-o",
+        dest="output_dir",
+        type=str,
+        default="./data",
+        help="Path to a directory where to save an output DL2 data file.",
     )
 
     args = parser.parse_args()
@@ -267,11 +287,11 @@ def main():
     # Process the input data:
     dl1_stereo_to_dl2(args.input_file_dl1, args.input_dir_rfs, args.output_dir)
 
-    logger.info('\nDone.')
+    logger.info("\nDone.")
 
     process_time = time.time() - start_time
-    logger.info(f'\nProcess time: {process_time:.0f} [sec]\n')
+    logger.info(f"\nProcess time: {process_time:.0f} [sec]\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
