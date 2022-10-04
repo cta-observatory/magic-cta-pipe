9426532579a545b9e00e267765a89e585b53f60d
Julian Sitarek <julian.sitarek@cp01.ctan.cta-observatory.org>
2022-10-04 22:45:51 +0000

    fixed two problems that prevented DL3 generation
    1) tel_combo was read as a float but used a index, and required cast to int
    this is a bit dangerous but it seems to be only used for the metadata
    2) list of telescopes was passed as an array, but instead should be a string

--- magicctapipe/io/gadf.py
+++ magicctapipe/io/gadf.py
@@ -133,10 +133,10 @@ def create_event_hdu(
 
     # Get the instruments used for the observation
     combo_types_unique = np.unique(event_table["combo_type"])
-    tel_combos = np.array(list(TEL_COMBINATIONS.keys()))[combo_types_unique]
+    tel_combos = np.array(list(TEL_COMBINATIONS.keys()))[combo_types_unique.astype(int)]
 
     tel_list = np.unique([tel_combo.split("_") for tel_combo in tel_combos])
-    instrument = "_".join(tel_list).upper()
+    instrument = "_".join(str(tel_list)).upper()
 
     # Transfer the RA/Dec directions to the galactic coordinate
     event_coords = SkyCoord(
