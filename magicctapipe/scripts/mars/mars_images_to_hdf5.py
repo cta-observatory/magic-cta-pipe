26eec9b21df3d2633a75f0ab4684909fdb18be75
Alessio Berti <alessioberti90@gmail.com>
2022-10-12 11:25:53 +0200

    Apply black to script.

--- magicctapipe/scripts/mars/mars_images_to_hdf5.py
+++ magicctapipe/scripts/mars/mars_images_to_hdf5.py
@@ -27,23 +27,17 @@ def parse_args(args):
     Parse command line options and arguments.
     """
 
-    parser = argparse.ArgumentParser(description="", prefix_chars='-')
+    parser = argparse.ArgumentParser(description="", prefix_chars="-")
     parser.add_argument(
-        "--calibrated",
-        action='store_true',
-        help="Save also calibrated images."
+        "--calibrated", action="store_true", help="Save also calibrated images."
     )
     parser.add_argument(
         "-in",
         "--input_mask",
-        nargs='?',
-        help='Mask for input files e.g. "20*_S_*.root" (NOTE: the double quotes should be there).'
+        nargs="?",
+        help='Mask for input files e.g. "20*_S_*.root" (NOTE: the double quotes should be there).',
     )
-    parser.add_argument(
-        "--max_events",
-        nargs='?',
-        help='Maximum number of events.'
-    )
+    parser.add_argument("--max_events", nargs="?", help="Maximum number of events.")
 
     return parser.parse_args(args)
 
@@ -134,8 +128,9 @@ def get_run_info_from_name(file_name):
         is_mc = True
     else:
         raise IndexError(
-            'Can not identify the run number and type (data/MC) of the file'
-            '{:s}'.format(file_name))
+            "Can not identify the run number and type (data/MC) of the file"
+            "{:s}".format(file_name)
+        )
 
     return run_number, is_mc, telescope, datalevel
 
@@ -146,7 +141,8 @@ class ImageContainerCalibrated(Container):
     tel_id = Field(-1, "Telescope ID")
     image_charge_calibrated = Field(
         None,
-        "Numpy array of pixel charges before cleaning, after calibration." "Shape: (n_pixel)",
+        "Numpy array of pixel charges before cleaning, after calibration."
+        "Shape: (n_pixel)",
         dtype=">f8",
         ndim=1,
     )
@@ -158,7 +154,8 @@ class ImageContainerCalibrated(Container):
     )
     image_time_calibrated = Field(
         None,
-        "Numpy array of pixel times before cleaning, after calibration." "Shape: (n_pixel)",
+        "Numpy array of pixel times before cleaning, after calibration."
+        "Shape: (n_pixel)",
         dtype=">f8",
         ndim=1,
     )
@@ -176,7 +173,9 @@ class ImageContainerCleaned(Container):
     tel_id = Field(-1, "Telescope ID")
     image_cleaned = Field(
         None,
-        "Numpy array of pixel charges after cleaning." "Shape: (n_pixel)", dtype=">f8", ndim=1,
+        "Numpy array of pixel charges after cleaning." "Shape: (n_pixel)",
+        dtype=">f8",
+        ndim=1,
     )
     image_time_cleaned = Field(
         None,
@@ -186,7 +185,15 @@ class ImageContainerCleaned(Container):
     )
 
 
-def build_image_container_calibrated(run_number, event_id, tel, image_charge_calibrated, image_charge_cleaned, image_time_calibrated, image_time_cleaned):
+def build_image_container_calibrated(
+    run_number,
+    event_id,
+    tel,
+    image_charge_calibrated,
+    image_charge_cleaned,
+    image_time_calibrated,
+    image_time_cleaned,
+):
     """
     Builds a ImageContainerCalibrated
 
@@ -222,7 +229,9 @@ def build_image_container_calibrated(run_number, event_id, tel, image_charge_cal
     # size, width, length, cog, slope, delta, leakage
 
 
-def build_image_container_cleaned(run_number, event_id, tel, image_charge_cleaned, image_time_cleaned):
+def build_image_container_cleaned(
+    run_number, event_id, tel, image_charge_cleaned, image_time_cleaned
+):
     """
     Builds a ImageContainerCleaned
 
@@ -277,7 +286,7 @@ def read_images(hdf5_files_mask, read_calibrated=False):
             try:
                 for image_container in reader.read(
                     table_name="/dl1/event/telescope/image/MAGIC/M1",
-                    containers=ImageContainerCalibrated()
+                    containers=ImageContainerCalibrated(),
                 ):
                     yield image_container
             except:
@@ -285,7 +294,7 @@ def read_images(hdf5_files_mask, read_calibrated=False):
             try:
                 for image_container in reader.read(
                     table_name="/dl1/event/telescope/image/MAGIC/M2",
-                    containers=ImageContainerCalibrated()
+                    containers=ImageContainerCalibrated(),
                 ):
                     yield image_container
             except:
@@ -294,7 +303,7 @@ def read_images(hdf5_files_mask, read_calibrated=False):
             try:
                 for image_container in reader.read(
                     table_name="/dl1/event/telescope/image/MAGIC/M1",
-                    containers=ImageContainerCleaned()
+                    containers=ImageContainerCleaned(),
                 ):
                     yield image_container
             except:
@@ -302,7 +311,7 @@ def read_images(hdf5_files_mask, read_calibrated=False):
             try:
                 for image_container in reader.read(
                     table_name="/dl1/event/telescope/image/MAGIC/M2",
-                    containers=ImageContainerCleaned()
+                    containers=ImageContainerCleaned(),
                 ):
                     yield image_container
             except:
@@ -327,18 +336,18 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
     for mars_file in mars_filelist:
 
         HDF5_ZSTD_FILTERS = tables.Filters(
-            complevel=5,            # enable compression, 5 is a good tradeoff between compression and speed
-            complib='blosc:zstd',   # compression using blosc/zstd
-            fletcher32=True,        # attach a checksum to each chunk for error correction
-            bitshuffle=False,       # for BLOSC, shuffle bits for better compression
+            complevel=5,  # enable compression, 5 is a good tradeoff between compression and speed
+            complib="blosc:zstd",  # compression using blosc/zstd
+            fletcher32=True,  # attach a checksum to each chunk for error correction
+            bitshuffle=False,  # for BLOSC, shuffle bits for better compression
         )
 
-        output_filename = Path(mars_file).with_suffix('.h5')
+        output_filename = Path(mars_file).with_suffix(".h5")
 
         with HDF5TableWriter(
             filename=output_filename,
-            group_name='dl1/event',
-            mode='a',
+            group_name="dl1/event",
+            mode="a",
             filters=HDF5_ZSTD_FILTERS,
             add_prefix=False,
             # overwrite=True,
@@ -365,7 +374,7 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
                             "UprootImageOrigClean",
                             "UprootTimeOrig",
                             "UprootTimeOrigClean",
-                            "MRawEvtHeader.fStereoEvtNumber"
+                            "MRawEvtHeader.fStereoEvtNumber",
                         ]
                     else:
                         branches = [
@@ -378,14 +387,14 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
                             "UprootImageOrigClean_2",
                             "UprootTimeOrig_2",
                             "UprootTimeOrigClean_2",
-                            "MRawEvtHeader_2.fStereoEvtNumber"
+                            "MRawEvtHeader_2.fStereoEvtNumber",
                         ]
                 else:
                     if datalevel == MARSDataLevel.STAR:
                         branches = [
                             "UprootImageOrigClean",
                             "UprootTimeOrigClean",
-                            "MRawEvtHeader.fStereoEvtNumber"
+                            "MRawEvtHeader.fStereoEvtNumber",
                         ]
                     else:
                         branches = [
@@ -394,15 +403,13 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
                             "MRawEvtHeader_1.fStereoEvtNumber",
                             "UprootImageOrigClean_2",
                             "UprootTimeOrigClean_2",
-                            "MRawEvtHeader_2.fStereoEvtNumber"
+                            "MRawEvtHeader_2.fStereoEvtNumber",
                         ]
 
                 events_count = 0
 
                 for batch in events.iterate(
-                        step_size="10 MB",
-                        expressions=branches,
-                        library="np"
+                    step_size="10 MB", expressions=branches, library="np"
                 ):
                     if events_count > max_events and max_events > 0:
                         break
@@ -417,23 +424,23 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
                                     np.array(batch["UprootImageOrig"][j]),
                                     np.array(batch["UprootImageOrigClean"][j]),
                                     np.array(batch["UprootTimeOrig"][j]),
-                                    np.array(batch["UprootTimeOrigClean"][j])
-                                    )
+                                    np.array(batch["UprootTimeOrigClean"][j]),
+                                )
                                 writer.write(
-                                    table_name=f'telescope/image/MAGIC/M{telescope}',
-                                    containers=image_container
+                                    table_name=f"telescope/image/MAGIC/M{telescope}",
+                                    containers=image_container,
                                 )
                             else:
                                 image_container = build_image_container_cleaned(
-                                        run_number,
-                                        batch["MRawEvtHeader.fStereoEvtNumber"][j],
-                                        telescope,
-                                        np.array(batch["UprootImageOrigClean"][j]),
-                                        np.array(batch["UprootImageOrigClean"][j]),
-                                    )
+                                    run_number,
+                                    batch["MRawEvtHeader.fStereoEvtNumber"][j],
+                                    telescope,
+                                    np.array(batch["UprootImageOrigClean"][j]),
+                                    np.array(batch["UprootImageOrigClean"][j]),
+                                )
                                 writer.write(
-                                    table_name=f'telescope/image/MAGIC/M{telescope}',
-                                    containers=image_container
+                                    table_name=f"telescope/image/MAGIC/M{telescope}",
+                                    containers=image_container,
                                 )
                             events_count += 1
                             if events_count > max_events and max_events > 0:
@@ -442,53 +449,53 @@ def save_images(mars_files_mask, save_calibrated=False, max_events=-1):
                         for j in range(len(batch["MRawEvtHeader_1.fStereoEvtNumber"])):
                             if save_calibrated:
                                 image_container = build_image_container_calibrated(
-                                        run_number,
-                                        batch["MRawEvtHeader_1.fStereoEvtNumber"][j],
-                                        1,
-                                        np.array(batch["UprootImageOrig_1"][j]),
-                                        np.array(batch["UprootImageOrigClean_1"][j]),
-                                        np.array(batch["UprootTimeOrig_1"][j]),
-                                        np.array(batch["UprootTimeOrigClean_1"][j]),
-                                    )
+                                    run_number,
+                                    batch["MRawEvtHeader_1.fStereoEvtNumber"][j],
+                                    1,
+                                    np.array(batch["UprootImageOrig_1"][j]),
+                                    np.array(batch["UprootImageOrigClean_1"][j]),
+                                    np.array(batch["UprootTimeOrig_1"][j]),
+                                    np.array(batch["UprootTimeOrigClean_1"][j]),
+                                )
                                 writer.write(
-                                    table_name='telescope/image/MAGIC/M1',
-                                    containers=image_container
+                                    table_name="telescope/image/MAGIC/M1",
+                                    containers=image_container,
                                 )
                                 image_container = build_image_container_calibrated(
-                                        run_number,
-                                        batch["MRawEvtHeader_2.fStereoEvtNumber"][j],
-                                        2,
-                                        np.array(batch["UprootImageOrig_2"][j]),
-                                        np.array(batch["UprootImageOrigClean_2"][j]),
-                                        np.array(batch["UprootTimeOrig_2"][j]),
-                                        np.array(batch["UprootTimeOrigClean_2"][j]),
-                                    )
+                                    run_number,
+                                    batch["MRawEvtHeader_2.fStereoEvtNumber"][j],
+                                    2,
+                                    np.array(batch["UprootImageOrig_2"][j]),
+                                    np.array(batch["UprootImageOrigClean_2"][j]),
+                                    np.array(batch["UprootTimeOrig_2"][j]),
+                                    np.array(batch["UprootTimeOrigClean_2"][j]),
+                                )
                                 writer.write(
-                                    table_name='telescope/image/MAGIC/M2',
-                                    containers=image_container
+                                    table_name="telescope/image/MAGIC/M2",
+                                    containers=image_container,
                                 )
                             else:
                                 image_container = build_image_container_cleaned(
-                                        run_number,
-                                        batch["MRawEvtHeader_1.fStereoEvtNumber"][j],
-                                        1,
-                                        np.array(batch["UprootImageOrigClean_1"][j]),
-                                        np.array(batch["UprootTimeOrigClean_1"][j]),
-                                    )
+                                    run_number,
+                                    batch["MRawEvtHeader_1.fStereoEvtNumber"][j],
+                                    1,
+                                    np.array(batch["UprootImageOrigClean_1"][j]),
+                                    np.array(batch["UprootTimeOrigClean_1"][j]),
+                                )
                                 writer.write(
-                                    table_name='telescope/image/MAGIC/M1',
-                                    containers=image_container
+                                    table_name="telescope/image/MAGIC/M1",
+                                    containers=image_container,
                                 )
                                 image_container = build_image_container_cleaned(
-                                        run_number,
-                                        batch["MRawEvtHeader_2.fStereoEvtNumber"][j],
-                                        2,
-                                        np.array(batch["UprootImageOrigClean_2"][j]),
-                                        np.array(batch["UprootTimeOrigClean_2"][j]),
-                                    )
+                                    run_number,
+                                    batch["MRawEvtHeader_2.fStereoEvtNumber"][j],
+                                    2,
+                                    np.array(batch["UprootImageOrigClean_2"][j]),
+                                    np.array(batch["UprootTimeOrigClean_2"][j]),
+                                )
                                 writer.write(
-                                    table_name='telescope/image/MAGIC/M2',
-                                    containers=image_container
+                                    table_name="telescope/image/MAGIC/M2",
+                                    containers=image_container,
                                 )
                             events_count += 1
                             if events_count > max_events and max_events > 0:
@@ -506,5 +513,5 @@ def main(*args):
     save_images(input_mask, save_calibrated, max_events)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(*sys.argv[1:])
