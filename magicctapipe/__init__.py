85f36a6abaaa4302387bac6624f82cd978056903
Alessio Berti <alessioberti90@gmail.com>
2022-11-12 16:28:25 +0100

    Add job to imported modules.

--- magicctapipe/__init__.py
+++ magicctapipe/__init__.py
@@ -1,6 +1,7 @@
 from .version import __version__
 from . import image
 from . import irfs
+from . import jobs
 from . import reco
 from . import scripts
 from . import utils
@@ -9,6 +10,7 @@ from . import utils
 __all__ = [
     "image",
     "irfs",
+    "jobs",
     "reco",
     "scripts",
     "utils",
