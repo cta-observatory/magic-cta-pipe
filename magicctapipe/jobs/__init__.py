97cc4b0445371c4e517aa0d105d535b5a88a2d3d
Alessio Berti <alessioberti90@gmail.com>
2022-11-14 15:24:20 +0100

    Add job creator for merge_hdf_files.py script.

--- magicctapipe/jobs/__init__.py
+++ magicctapipe/jobs/__init__.py
@@ -1,3 +1,4 @@
 from .magic_calib_to_dl1_jobs import magic_calib_to_dl1_jobs
+from .merge_hdf_files_jobs import merge_hdf_files_jobs
 
-__all__ = ["magic_calib_to_dl1_jobs"]
+__all__ = ["magic_calib_to_dl1_jobs", "merge_hdf_files_jobs"]
