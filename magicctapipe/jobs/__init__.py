fb145c45d109d30ed2ca0462a734d4e6b6850fbb
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-12-05 16:46:00 +0100

    Added job creators

--- magicctapipe/jobs/__init__.py
+++ magicctapipe/jobs/__init__.py
@@ -1,11 +1,23 @@
 from .magic_calib_to_dl1_jobs import magic_calib_to_dl1_jobs
+from .lst1_magic_stereo_reco_jobs import lst1_magic_stereo_reco_jobs
+from .lst1_magic_dl1_stereo_to_dl2_jobs import lst1_magic_dl1_stero_to_dl2_jobs
+from .lst1_magic_dl2_to_dl3_jobs import lst1_magic_dl2_to_dl3_jobs
 from .merge_hdf_files_jobs import merge_hdf_files_jobs
 from .lst1_magic_event_coincidence_jobs import lst1_magic_event_coincidence_jobs
-from .lst1_magic_stereo_reco_jobs import lst1_magic_stereo_reco_jobs
+from .lst1_magic_mc_dl0_to_dl1_jobs import lst1_magic_mc_dl0_to_dl1_jobs
+from .lst1_magic_create_dl3_index_files_jobs import lst1_magic_create_dl3_index_files_jobs
+from .lst1_magic_create_irf_jobs import lst1_magic_create_irf_jobs
+from .lst1_magic_train_rfs_jobs import lst1_magic_train_rfs_jobs
 
 __all__ = [
     "magic_calib_to_dl1_jobs",
     "merge_hdf_files_jobs",
     "lst1_magic_event_coincidence_jobs",
+    "lst1_magic_mc_dl0_to_dl1_jobs"
     "lst1_magic_stereo_reco_jobs",
+    "lst1_magic_dl1_stero_to_dl2_jobs",
+    "lst1_magic_dl2_to_dl3_jobs",
+    "lst1_magic_create_dl3_index_files_jobs",
+    "lst1_magic_create_irf_jobs",
+    "lst1_magic_train_rfs_jobs",
 ]
