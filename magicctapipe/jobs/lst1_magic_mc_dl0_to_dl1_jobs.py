1e23ad1f536a9d06e09e235582e86e1325005a79
Elli Sophie Jobst <elli.jobst@gmx.de>
2022-12-05 16:44:28 +0100

    Add job creator for lst1_magic_mc_dl0_to_dl1.py script

---
+++ magicctapipe/jobs/lst1_magic_mc_dl0_to_dl1_jobs.py
@@ -0,0 +1,115 @@
+import os
+import argparse
+import glob
+
+import subprocess as sp
+
+from jinja2 import Template
+from pathlib import Path
+
+__all__ = ["lst1_magic_mc_dl0_to_dl1_jobs"]
+
+template = """#!/bin/bash
+#SBATCH -A aswg
+#SBATCH -p short,long,xxl
+#SBATCH -J {{ job_name }}_%A
+#SBATCH -o {{ output_dir }}/{{ job_name }}_%A_%a.out
+#SBATCH -e {{ output_dir }}/{{ job_name }}_%A_%a.err
+#SBATCH --array=0-{{ stop_job }}
+
+source {{ home }}/.bashrc
+conda activate magic-lst1
+
+case $SLURM_ARRAY_TASK_ID in
+{% for infile in file_list %}
+    {{ loop.index0 }}) INPUT_FILE={{ infile }};;
+{% endfor %}
+esac
+
+lst1_magic_mc_dl0_to_dl1 -i $INPUT_FILE -o {{ output_dir }} -c {{ config_file }}
+
+"""
+
+def lst1_magic_mc_dl0_to_dl1_jobs(
+	input_files, output_dir, config_file, submit
+):
+	file_list = [
+        f"{str(Path(filename).parent.resolve())}/{Path(filename).name}"
+        for filename in sorted(glob.glob(input_files))
+    ]
+
+    n_jobs = len(file_list)
+
+    job_dict = {
+        "job_name": "lst1_magic_mc_dl0_to_dl1",
+        "stop_job": n_jobs - 1,
+        "home": os.environ["HOME"],
+        "file_list": file_list,
+        "output_dir": str(Path(output_dir).resolve()),
+        "config_file": f"{str(Path(config_file).parent.resolve())}/{Path(config_file).name}",
+    }
+
+    j2_template = Template(template)
+    print(j2_template.render(job_dict))
+    job_submit_filename = "lst1_magic_mc_dl0_to_dl1.slurm"
+    j2_template.stream(job_dict).dump(job_submit_filename)
+
+    if submit:
+        commandargs = ["sbatch", job_submit_filename]
+        sp.check_output(commandargs, shell=False)
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Script to create and optionally submit jobs for lst1_magic_mc_dl0_to_dl1 script.",
+        prefix_chars="-",
+    )
+
+    parser.add_argument(
+        "--input-file_dl1",
+        "-i",
+        dest="input_file",
+        type=str,
+        required=True,
+        help="Path to input DL0 files",
+    )
+
+    parser.add_argument(
+        "--output-dir",
+        "-o",
+        dest="output_dir",
+        type=str,
+        default="./data",
+        help="Path to a directory where to save an output DL1 data file",
+    )
+
+    parser.add_argument(
+        "--config-file",
+        "-c",
+        dest="config_file",
+        type=str,
+        default="./config.yaml",
+        help="Path to a configuration file",
+    )
+
+    parser.add_argument(
+        "--submit",
+        dest="submit",
+        action="store_true",
+        help="Submit the job via SLURM sbatch",
+    )
+
+    flags = parser.parse_args()
+
+    input_file = flags.input_file
+    output_dir = flags.output_dir
+    config_file = flags.config_file
+    submit = flags.submit
+
+    lst1_magic_mc_dl0_to_dl1_jobs(
+        input_file, output_dir,config_file, submit
+    )
+
+
+if __name__ == "__main__":
+    main()
