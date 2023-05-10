46c7fc103f512b1fd6d8b7a15d49f2f962e1284e
Elisa-Visentin <121040436+Elisa-Visentin@users.noreply.github.com>
2023-05-10 10:54:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         "ctapipe~=0.19.0",
         "ctaplot~=0.5.5",
+        'ctapipe_io_magic~=0.5.1',
         "gammapy~=0.19.0",
         "uproot",
         "pandas",
