e2adffe5cd571f9904d4359ef7bd07331c37a3fa
Elisa-Visentin <121040436+Elisa-Visentin@users.noreply.github.com>
2023-10-20 15:45:03 +0200

    Update test_mc_dl0_dl1.py

--- magicctapipe/scripts/lst1_magic/tests/test_mc_dl0_dl1.py
+++ magicctapipe/scripts/lst1_magic/tests/test_mc_dl0_dl1.py
@@ -1,5 +1,4 @@
 import subprocess
-import pytest
 import glob
 
 
@@ -9,7 +8,7 @@ def test_mc_dl0_dl1(temp_DL1_gamma, dl0_gamma, config):
     """
 
     for file in dl0_gamma:
-        out=subprocess.run(
+        _ =subprocess.run(
             [
                 "lst1_magic_mc_dl0_to_dl1",
                 f"-i{str(file)}",
@@ -18,12 +17,8 @@ def test_mc_dl0_dl1(temp_DL1_gamma, dl0_gamma, config):
             ]
         )
     
-        if out.returncode != 0:
-            raise ValueError(
-                f"MC DL0 to DL1 script failed with return code {out.returncode} for file {file}"
-            )    
-
-    assert len(glob.glob(f"{temp_DL1_gamma}/*.h5"))>0
+       
+    assert len(glob.glob(f"{temp_DL1_gamma}/*.h5"))==4
 
 
 def test_mc_dl0_dl1_focal_exc(temp_DL1_gamma_focal_exc, dl0_gamma, config):
@@ -32,7 +27,7 @@ def test_mc_dl0_dl1_focal_exc(temp_DL1_gamma_focal_exc, dl0_gamma, config):
     """
 
     for file in dl0_gamma:
-        out=subprocess.run(
+        _ =subprocess.run(
             [
                 "lst1_magic_mc_dl0_to_dl1",
                 f"-i{str(file)}",
