a592a24ca412d64c3952951c111a07cb43bcad0e
Alessio Berti <alessioberti90@gmail.com>
2023-10-27 15:26:52 +0200

    Remove unused function to compute image parameters.

--- magicctapipe/image/__init__.py
+++ magicctapipe/image/__init__.py
@@ -2,7 +2,6 @@ from .cleaning import (
     MAGICClean,
     PixelTreatment,
     get_num_islands_MAGIC,
-    clean_image_params,
 )
 
 from .leakage import (
@@ -13,6 +12,5 @@ __all__ = [
     "MAGICClean",
     "PixelTreatment",
     "get_num_islands_MAGIC",
-    "clean_image_params",
     "get_leakage",
 ]
