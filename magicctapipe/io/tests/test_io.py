7ebfe1ac50aa5aff78b2efe164281cda4522161a
Elisa-Visentin <121040436+Elisa-Visentin@users.noreply.github.com>
2023-12-11 16:40:19 +0100

    Update test_io.py

--- magicctapipe/io/tests/test_io.py
+++ magicctapipe/io/tests/test_io.py
@@ -262,7 +262,7 @@ def test_load_train_data_files_g(gamma_stereo):
     """
 
     events = load_train_data_files(str(gamma_stereo[0]))
-    assert list(events.keys()) == ["LST1_M1", "LST1_M1_M2", "LST1_M2"]
+    assert list(events.keys()) == ["LST1_M1", "LST1_M1_M2", "LST1_M2", "M1_M2"]
     data = events["LST1_M1"]
     assert np.all(data["combo_type"] == 0)
     assert "off_axis" in data.columns
