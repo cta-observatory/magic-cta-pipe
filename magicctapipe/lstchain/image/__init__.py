24f4bcef49f1b4f1f5e58f41ffaf719d4450eb9c
Alessio Berti <alessioberti90@gmail.com>
2024-01-05 18:31:39 +0100

    Remove lstchain submodule.

--- magicctapipe/lstchain/image/__init__.py
+++
@@ -1,9 +0,0 @@
-from .cleaning import apply_dynamic_cleaning
-from .modifier import add_noise_in_pixels, random_psf_smearer, set_numba_seed
-
-__all__ = [
-    "apply_dynamic_cleaning",
-    "add_noise_in_pixels",
-    "random_psf_smearer",
-    "set_numba_seed",
-]
