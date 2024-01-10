d43de12b2d24c45b974c1af35baf5c383aa18485
Alessio Berti <alessioberti90@gmail.com>
2024-01-10 11:26:07 +0100

    Redefine reflected camera geometry for MARS.

--- magicctapipe/utils/camera_geometry.py
+++ magicctapipe/utils/camera_geometry.py
@@ -1,48 +1,15 @@
 """
 Utilities for camera geometry transformations
 """
+import astropy.units as u
 from ctapipe.instrument import CameraGeometry
 
 __all__ = [
-    "scale_camera_geometry",
-    "reflected_camera_geometry",
+    "reflected_camera_geometry_mars",
 ]
 
 
-def scale_camera_geometry(camera_geom, factor):
-    """Scale given camera geometry of a given (constant) factor
-
-    Parameters
-    ----------
-    camera_geom : ctapipe.instrument.camera.geometry.CameraGeometry
-        Camera geometry
-    factor : float
-        Scale factor
-
-    Returns
-    -------
-    ctapipe.instrument.camera.geometry.CameraGeometry
-        Scaled camera geometry
-    """
-    pix_x_scaled = factor * camera_geom.pix_x
-    pix_y_scaled = factor * camera_geom.pix_y
-    pix_area_scaled = camera_geom.guess_pixel_area(
-        pix_x_scaled, pix_y_scaled, camera_geom.pix_type
-    )
-
-    return CameraGeometry(
-        camera_name="MAGICCam",
-        pix_id=camera_geom.pix_id,
-        pix_x=pix_x_scaled,
-        pix_y=pix_y_scaled,
-        pix_area=pix_area_scaled,
-        pix_type=camera_geom.pix_type,
-        pix_rotation=camera_geom.pix_rotation,
-        cam_rotation=camera_geom.cam_rotation,
-    )
-
-
-def reflected_camera_geometry(camera_geom):
+def reflected_camera_geometry_mars(camera_geom):
     """Reflect camera geometry (x->-y, y->-x)
 
     Parameters
@@ -57,14 +24,12 @@ def reflected_camera_geometry(camera_geom):
     """
 
     return CameraGeometry(
-        camera_name="MAGICCam",
+        name="MAGICCam",
         pix_id=camera_geom.pix_id,
         pix_x=-1.0 * camera_geom.pix_y,
         pix_y=-1.0 * camera_geom.pix_x,
-        pix_area=camera_geom.guess_pixel_area(
-            camera_geom.pix_x, camera_geom.pix_y, camera_geom.pix_type
-        ),
+        pix_area=camera_geom.pix_area,
         pix_type=camera_geom.pix_type,
-        pix_rotation=camera_geom.pix_rotation,
+        pix_rotation=30 * u.deg - camera_geom.pix_rotation,
         cam_rotation=camera_geom.cam_rotation,
     )
