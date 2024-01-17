dc89b9217ca8be8fb747c148ff74811c18773108
Alessio Berti <alessioberti90@gmail.com>
2024-01-17 19:19:20 +0100

    Use correct list for telescope combinations.

--- magicctapipe/io/tests/test_io.py
+++ magicctapipe/io/tests/test_io.py
@@ -271,7 +271,7 @@ class TestStereoMC:
         """
 
         events = load_train_data_files(str(p_stereo[0]))
-        assert list(events.keys()) == ["M1_M2", "LST1_M1", "LST1_M2", "LST1_M1_M2"]
+        assert list(events.keys()) == ["LST1_M1", "LST1_M1_M2", "LST1_M2", "M1_M2"]
         data = events["LST1_M1"]
         assert np.all(data["combo_type"] == 1)
         assert "off_axis" in data.columns
@@ -283,7 +283,7 @@ class TestStereoMC:
         """
 
         events = load_train_data_files(str(gamma_stereo[0]))
-        assert list(events.keys()) == ["M1_M2", "LST1_M1", "LST1_M2", "LST1_M1_M2"]
+        assert list(events.keys()) == ["LST1_M1", "LST1_M1_M2", "LST1_M2", "M1_M2"]
         data = events["LST1_M1"]
         assert np.all(data["combo_type"] == 1)
         assert "off_axis" in data.columns
