8489c33ab5485bd0590e8e4064b400049cd66ee4
Alessio Berti <alessioberti90@gmail.com>
2024-02-22 11:44:06 +0100

    Use the impact distance calculation from ctapipe.

--- magicctapipe/scripts/lst1_magic/lst1_magic_stereo_reco.py
+++ magicctapipe/scripts/lst1_magic/lst1_magic_stereo_reco.py
@@ -55,7 +55,7 @@ from magicctapipe.io import (
     get_stereo_events,
     save_pandas_data_in_table,
 )
-from magicctapipe.utils import calculate_impact, calculate_mean_direction
+from magicctapipe.utils import calculate_mean_direction
 
 __all__ = ["calculate_pointing_separation", "stereo_reconstruction"]
 
@@ -331,17 +331,6 @@ def stereo_reconstruction(input_file, output_dir, config, magic_only_analysis=Fa
         stereo_params.az.wrap_at("360 deg", inplace=True)
 
         for tel_id in tel_ids:
-            # Calculate the impact parameter
-            impact = calculate_impact(
-                shower_alt=stereo_params.alt,
-                shower_az=stereo_params.az,
-                core_x=stereo_params.core_x,
-                core_y=stereo_params.core_y,
-                tel_pos_x=tel_positions[tel_id][0],
-                tel_pos_y=tel_positions[tel_id][1],
-                tel_pos_z=tel_positions[tel_id][2],
-            )
-
             # Set the stereo parameters to the data frame
             params = {
                 "alt": stereo_params.alt.to_value("deg"),
@@ -350,7 +339,7 @@ def stereo_reconstruction(input_file, output_dir, config, magic_only_analysis=Fa
                 "az_uncert": stereo_params.az_uncert.to_value("deg"),
                 "core_x": stereo_params.core_x.to_value("m"),
                 "core_y": stereo_params.core_y.to_value("m"),
-                "impact": impact.to_value("m"),
+                "impact": event.dl2.tel[tel_id].impact.distance.to_value("m"),
                 "h_max": stereo_params.h_max.to_value("m"),
             }
 
