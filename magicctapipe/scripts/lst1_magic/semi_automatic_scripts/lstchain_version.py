b6937b5f078746e03970dda4d0e69f8e811a0f68
Elisa-Visentin <elisa.visentin@edu.unito.it>
2024-04-29 08:44:37 +0000

    linter

--- magicctapipe/scripts/lst1_magic/semi_automatic_scripts/lstchain_version.py
+++ magicctapipe/scripts/lst1_magic/semi_automatic_scripts/lstchain_version.py
@@ -3,12 +3,12 @@ Fill the lstchain_0.9 and lstchain_0.10 columns of the LST database (i.e., which
 """
 
 
-import pandas as pd
 import os
+
+import pandas as pd
 
 
 def main():
-    
 
     """
     Main function
@@ -19,54 +19,32 @@ def main():
         key="joint_obs",
     )
     for i, row in df_LST.iterrows():
-       
-        lst_9 = False
-        lst_10=False
-        run=row['LST1_run']
-        run=format(int(run), '05d')
-        date=row['DATE']
-        
-        if os.path.isfile(f'/fefs/aswg/data/real/DL1/{date}/v0.9/tailcut84/dl1_LST-1.Run{run}.h5'):
-            lst_9=True
-        if os.path.isfile(f'/fefs/aswg/data/real/DL1/{date}/v0.10/tailcut84/dl1_LST-1.Run{run}.h5'):
-            lst_10=True
-        if (lst_9==False) and (lst_10==False):
-            df_LST.at[i,'error_code'] = '002'
-        df_LST.at[i,'lstchain_0.9'] = lst_9
-        df_LST.at[i,'lstchain_0.10'] = lst_10
-        
 
-    
+        lst_9 = False
+        lst_10 = False
+        run = row["LST1_run"]
+        run = format(int(run), "05d")
+        date = row["DATE"]
+
+        if os.path.isfile(
+            f"/fefs/aswg/data/real/DL1/{date}/v0.9/tailcut84/dl1_LST-1.Run{run}.h5"
+        ):
+            lst_9 = True
+        if os.path.isfile(
+            f"/fefs/aswg/data/real/DL1/{date}/v0.10/tailcut84/dl1_LST-1.Run{run}.h5"
+        ):
+            lst_10 = True
+        if (lst_9 == False) and (lst_10 == False):
+            df_LST.at[i, "error_code"] = "002"
+        df_LST.at[i, "lstchain_0.9"] = lst_9
+        df_LST.at[i, "lstchain_0.10"] = lst_10
+
     df_LST.to_hdf(
         "/fefs/aswg/workspace/elisa.visentin/auto_MCP_PR/observations_LST.h5",
         key="joint_obs",
         mode="w",
     )
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
