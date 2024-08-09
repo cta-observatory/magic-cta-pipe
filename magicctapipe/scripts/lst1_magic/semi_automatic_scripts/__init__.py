04e6d6f9553380ecde303f52408c7658bb48de73
Elisa-Visentin <elisa.visentin@edu.unito.it>
2024-08-09 13:26:16 +0000

    remove MC

--- magicctapipe/scripts/lst1_magic/semi_automatic_scripts/__init__.py
+++ magicctapipe/scripts/lst1_magic/semi_automatic_scripts/__init__.py
@@ -2,29 +2,22 @@ from .clusters import slurm_lines
 from .coincident_events import configfile_coincidence, linking_bash_lst
 from .dl1_production import (
     config_file_gen,
-    directories_generator_MC,
     directories_generator_real,
     lists_and_bash_gen_MAGIC,
-    lists_and_bash_generator,
 )
 from .merge_stereo import MergeStereo
-from .merging_runs import merge, mergeMC, split_train_test
-from .stereo_events import bash_stereo, bash_stereoMC, configfile_stereo
+from .merging_runs import merge
+from .stereo_events import bash_stereo, configfile_stereo
 
 __all__ = [
-    "split_train_test",
     "merge",
-    "mergeMC",
     "config_file_gen",
-    "lists_and_bash_generator",
     "lists_and_bash_gen_MAGIC",
     "directories_generator_real",
-    "directories_generator_MC",
     "configfile_coincidence",
     "linking_bash_lst",
     "configfile_stereo",
     "bash_stereo",
-    "bash_stereoMC",
     "slurm_lines",
     "MergeStereo",
 ]
