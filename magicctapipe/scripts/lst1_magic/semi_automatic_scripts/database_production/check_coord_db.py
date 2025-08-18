0b3db86151f499074ed0cc7c89d4e6aa7bdfed00
Elisa-Visentin <elisa.visentin@edu.unito.it>
2025-08-18 10:27:34 +0000

    Fix bugs and add eatures

--- magicctapipe/scripts/lst1_magic/semi_automatic_scripts/database_production/check_coord_db.py
+++ magicctapipe/scripts/lst1_magic/semi_automatic_scripts/database_production/check_coord_db.py
@@ -1,27 +1,27 @@
 """
-Check if source coordinates stored in the LSt database 
-(from astropy or from a json dictionary) are consistehnt 
-with the ones stored in one of the MAGIC files for each 
+Check if source coordinates stored in the LST database
+(from astropy or from a json dictionary) are consistent
+with the ones stored in one of the MAGIC files for each
 source. Note: only one MAGIC subrun per source is used!
 
 Usage:
 $ check_coord_db (-c config_auto_MCP.yaml)
 """
 
-import os
+import glob
+import math
 
 import numpy as np
 import pandas as pd
-import yaml
-import glob
 import uproot
-import math
+import yaml
 
 from magicctapipe.io import resource_file
 from magicctapipe.utils import auto_MCP_parser
 
 degrees_per_hour = 15.0
 seconds_per_hour = 3600.0
+
 
 def main():
 
@@ -31,7 +31,7 @@ def main():
 
     parser = auto_MCP_parser()
     args = parser.parse_args()
-    
+
     with open(
         args.config_file, "rb"
     ) as f:  # "rb" mode opens the file in binary format for reading
@@ -48,7 +48,7 @@ def main():
 
     lst_h5 = config_dict["database_paths"]["LST"]
     lst_key = config_dict["database_keys"]["LST"]
-    lst_df=pd.read_hdf(lst_h5, lst_key)
+    lst_df = pd.read_hdf(lst_h5, lst_key)
     df = pd.read_hdf(
         config_dict["database_paths"]["MAGIC+LST1"],
         key=config_dict["database_keys"]["MAGIC+LST1"],
@@ -58,8 +58,9 @@ def main():
         key=config_dict["database_keys"]["MAGIC+LST1_bis"],
     )  # TODO: put this file in a shared folder
     df = pd.concat([df, df2]).drop_duplicates(subset="LST1_run", keep="first")
-    
-    df=df.drop_duplicates(subset=['source'])
+
+    df = df.drop_duplicates(subset=["source"])
+    df = df.reset_index()
 
     date_lst = pd.to_datetime(df["DATE"], format="%Y%m%d")
 
@@ -69,38 +70,59 @@ def main():
     date_magic = date_magic.dt.strftime("%Y/%m/%d").to_list()
 
     for i in range(len(df)):
-        magic_first_run = (df["MAGIC_runs"].to_list())[i].rstrip("]").lstrip("[").split(", ")[0]
+        magic_first_run = (
+            (df["MAGIC_runs"].to_list())[i].rstrip("]").lstrip("[").split(", ")[0]
+        )
         lst_run = (df["LST1_run"].to_list())[i]
         source = (df["source"].to_list())[i]
+        if (
+            len(
+                glob.glob(
+                    f"/fefs/onsite/common/MAGIC/data/M[12]/event/Calibrated/{date_magic[i]}/*{magic_first_run}*{source}*.root"
+                )
+            )
+            == 0
+        ):
+            print(
+                f"No MAGIC file for {source}, {date_magic[i]} (MAGIC convention), run {magic_first_run}\n\n"
+            )
+            continue
         magic_file = glob.glob(
             f"/fefs/onsite/common/MAGIC/data/M[12]/event/Calibrated/{date_magic[i]}/*{magic_first_run}*{source}*.root"
-        ) [0]
-        ra_db= lst_df.loc[lst_df['LST1_run']==lst_run][0]['ra']
-        dec_db= lst_df.loc[lst_df['LST1_run']==lst_run][0]['dec']
-        file_tree=uproot.open(magic_file)
+        )[0]
+
+        if len(lst_df.loc[lst_df["LST1_run"] == lst_run]) == 0:
+            print(f"{source}, run {lst_run}, not in LST database\n\n")
+            continue
+        ra_db = lst_df.loc[lst_df["LST1_run"] == lst_run]["ra"].values[0]
+        dec_db = lst_df.loc[lst_df["LST1_run"] == lst_run]["dec"].values[0]
+
+        if (np.isnan(ra_db)) or (np.isnan(dec_db)):
+            print(f"RA or Dec is NaN in the database for {source}\n\n")
+            continue
+
+        file_tree = uproot.open(magic_file)
         branch_list = [
             "MRawRunHeader.fSourceRA",
             "MRawRunHeader.fSourceDEC",
         ]
-        coord = magic_file["RunHeaders"].arrays(
-            branch_list, library="np"
+        coord = file_tree["RunHeaders"].arrays(branch_list, library="np")
+        ra_file = (
+            coord["MRawRunHeader.fSourceRA"][0] / seconds_per_hour * degrees_per_hour
         )
-        ra_file=coord["MRawRunHeader.fSourceRA"][0] / seconds_per_hour* degrees_per_hour
-    
+        dec_file = coord["MRawRunHeader.fSourceDEC"][0] / seconds_per_hour
 
-        dec_file=coord["MRawRunHeader.fSourceDEC"][0]/ seconds_per_hour 
-        if (math.isclose(ra_db, ra_file, abs_tol=0.001)) and (math.isclose(dec_db, dec_file, abs_tol=0.001)):
+        if (math.isclose(ra_db, ra_file, abs_tol=0.02)) and (
+            math.isclose(dec_db, dec_file, abs_tol=0.02)
+        ):
             continue
         else:
-            print(f'coordinates not equal in MAGIc Calibrated files and in LST database for {source}')
-            print(f'database RA = {ra_db}, DEC ={dec_db}; MAGIC ROOT files RA = {ra_file}, DEC = {dec_file}')
-
-
-
-
-
-
-    
+            print(
+                f"Coordinates not equal in MAGIC Calibrated files and in LST database for {source}"
+            )
+            print(
+                f"Database RA = {ra_db}, DEC ={dec_db}; MAGIC ROOT files RA = {ra_file}, DEC = {dec_file}\n\n"
+            )
 
 
 if __name__ == "__main__":
